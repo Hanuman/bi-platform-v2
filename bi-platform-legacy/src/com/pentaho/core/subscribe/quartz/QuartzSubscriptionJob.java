08e802f6333e60799d4a0615a5e5ade6180abf1c
dkincade <dkincade@5d404267-db13-0410-8476-b1f6b398e63a>
2008-08-28 01:26:14 +0000

    Moving the bi-platform-legacy project under trunk
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@31503 5d404267-db13-0410-8476-b1f6b398e63a

---
+++ bi-platform-legacy/src/com/pentaho/core/subscribe/quartz/QuartzSubscriptionJob.java
@@ -0,0 +1,32 @@
+/*
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
+ * BI Platform.  The Initial Developer is Pentaho Corporation.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+ *
+ * Copyright 2008 Pentaho Corporation.  All rights reserved. 
+ * 
+ * @created June 09, 2008 
+ * @author mbatchel
+ */
+package com.pentaho.core.subscribe.quartz;
+
+/**
+ * @deprecated
+ * @author mbatchel
+ *
+ * This class is here only to support existing Quartz schedules. Please don't
+ * directly use this class.
+ *
+ */
+public class QuartzSubscriptionJob extends org.pentaho.platform.scheduler.QuartzSubscriptionJob {
+
+  public QuartzSubscriptionJob() {
+    super();
+  }
+}
---
+++ bi-platform-legacy/src/org/pentaho/plugin/jfreereport/helper/PentahoResourceLoader.java
@@ -0,0 +1,16 @@
+package org.pentaho.plugin.jfreereport.helper;
+
+/**
+ * 
+ * This class is only here for backward compatibility
+ * 
+ * @deprecated
+ * @author mbatchel
+ *
+ */
+public class PentahoResourceLoader extends org.pentaho.platform.plugin.action.jfreereport.helper.PentahoResourceLoader {
+
+  public PentahoResourceLoader() {
+    super();
+  }
+}
