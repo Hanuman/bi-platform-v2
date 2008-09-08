0234fbed57e61f591969ff17c57847ab0d58f62a
nbaker <nbaker@5d404267-db13-0410-8476-b1f6b398e63a>
2008-09-08 15:54:51 +0000

    Mantle hooks added
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@31959 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-appserver/webapps/pentaho/adhoc/js/controller/RepositoryBrowserController.js
+++ bi-platform-appserver/webapps/pentaho/adhoc/js/controller/RepositoryBrowserController.js
@@ -19,15 +19,15 @@
  */
 RepositoryBrowserController = function( wiz, solutionRepository )
 {
-	this.wiz = wiz;
-	this.reportSpec = null;
-	this.solutionRepository = solutionRepository;
-	this.repositoryBrowser = null;
-	this.solution = null;
-	this.path = null;
-	this.filename = null;
-	this.bIsSaving = false;
-	this.onAfterSaveCallback = null;
+  this.wiz = wiz;
+  this.reportSpec = null;
+  this.solutionRepository = solutionRepository;
+  this.repositoryBrowser = null;
+  this.solution = null;
+  this.path = null;
+  this.filename = null;
+  this.bIsSaving = false;
+  this.onAfterSaveCallback = null;
 }
 
 /*static*/RepositoryBrowserController.RE_FILE_FILTER = /^.*\.waqr\.xaction$/;
@@ -35,195 +35,218 @@ RepositoryBrowserController = function( wiz, solutionRepository )
 
 RepositoryBrowserController.prototype.setReportSpec = function( reportSpec )
 {
-	this.reportSpec = reportSpec;
+  this.reportSpec = reportSpec;
 }
 
 RepositoryBrowserController.prototype.init = function()
 {
   var rootFolderPathData = { 
-  	  isDir: true,
-  	  solution: SolutionRepository.EMPTY_SOLUTION_NAME,
-  	  path: SolutionRepository.EMPTY_FOLDER, 
-  	  name: SolutionRepository.ROOT_FOLDER, 
-  	  displayName: SolutionRepository.ROOT_FOLDER,
-  	  description: Messages.getString("SOLUTION_REPOSITORY")
+      isDir: true,
+      solution: SolutionRepository.EMPTY_SOLUTION_NAME,
+      path: SolutionRepository.EMPTY_FOLDER, 
+      name: SolutionRepository.ROOT_FOLDER, 
+      displayName: SolutionRepository.ROOT_FOLDER,
+      description: Messages.getString("SOLUTION_REPOSITORY")
     };
   var localThis = this;
-	this.repositoryBrowser = new WaqrRepositoryBrowser( Messages.getString("saveAsBtnTxt"),
+  this.repositoryBrowser = new WaqrRepositoryBrowser( Messages.getString("saveAsBtnTxt"),
     this.solutionRepository, rootFolderPathData,
-		function( solution, path )
-		{
-		  localThis.handleFolderChange( solution, path );
-		}
-	);
-	this.repositoryBrowser.setOnSaveHandler( this.handleSave, this );
-	this.repositoryBrowser.setOnCancelHandler( this.handleCancel, this );
+    function( solution, path )
+    {
+      localThis.handleFolderChange( solution, path );
+    }
+  );
+  this.repositoryBrowser.setOnSaveHandler( this.handleSave, this );
+  this.repositoryBrowser.setOnCancelHandler( this.handleCancel, this );
 }
-RepositoryBrowserController.isValidFilename = function( filename )	
+RepositoryBrowserController.isValidFilename = function( filename )  
 {
-	var match = filename.match( RepositoryBrowserController.RE_WAQR_INVALID_FILENAME );
-	return null == match;
+  var match = filename.match( RepositoryBrowserController.RE_WAQR_INVALID_FILENAME );
+  return null == match;
 }
 RepositoryBrowserController.prototype.handleSave = function()
 {
-	var filename = this.repositoryBrowser.getSaveAsName();
-	var solution = this.repositoryBrowser.getSolutionName();
-	var path = this.repositoryBrowser.getPath();
-	
-	var bSaveOk = this.saveReportSpecAs( filename, solution, path, false );
-	
-	return bSaveOk;
+  var filename = this.repositoryBrowser.getSaveAsName();
+  var solution = this.repositoryBrowser.getSolutionName();
+  var path = this.repositoryBrowser.getPath();
+  
+  var bSaveOk = this.saveReportSpecAs( filename, solution, path, false );
+  
+  return bSaveOk;
 }
 
 RepositoryBrowserController.prototype.handleFolderChange = function( solution, path )
 {
   var localThis = this;
-	this.solutionRepository.getSolutionFolderChildren( solution, path, 
-		RepositoryBrowserController.RE_FILE_FILTER, /*async*/true,
-		function( children )
-		{
-			localThis.repositoryBrowser.loadListCtrl( children );
+  this.solutionRepository.getSolutionFolderChildren( solution, path, 
+    RepositoryBrowserController.RE_FILE_FILTER, /*async*/true,
+    function( children )
+    {
+      localThis.repositoryBrowser.loadListCtrl( children );
       localThis.repositoryBrowser.tryToEnableSaveBtn();
-		}
-	);
+    }
+  );
 };
 
 RepositoryBrowserController.prototype.handleCancel = function()
 {
-	this.wiz.getSaveBtn().setEnabled( true );
-	this.wiz.getSaveAsBtn().setEnabled( true );
+  this.wiz.getSaveBtn().setEnabled( true );
+  this.wiz.getSaveAsBtn().setEnabled( true );
+}
+
+RepositoryBrowserController.prototype.getOutputType = function(){
+  if(this.requestedOutputType != null){
+    return this.requestedOutputType;
+  } else {
+    return this.repositoryBrowser.getOutputType();
+  }
+}
+
+RepositoryBrowserController.prototype.remoteSave= function( filename,
+  solution, path, type, bOverwrite )
+{
+  this.requestedOutputType = type;
+  this.saveReportSpecAs(filename, solution, path, bOverwrite);
 }
 
 /*private*/RepositoryBrowserController.prototype.saveReportSpecAs = function( filename,
-	solution, path, bOverwrite )
+  solution, path, bOverwrite )
 {
-	if ( RepositoryBrowserController.isValidFilename( filename ) )
-	{
-  	/*
-  	 * if bOverwrite is false, check to see if the file exists in the client solutionRepository. If
-  	 * the file doesn't exist, we can safely set bOverwrite to false, that way if the file
-  	 * somehow gets created by another process before we save, the server will still
-  	 * detect it and return an error msg preventing an overwrite.
-  	 * If the file does exist, prompt the user for overwrite.
-  	 */
-  	if ( !bOverwrite )
-  	{
-  		var bExists = this.solutionRepository.doesSolutionFileExist( solution, path, filename + SolutionRepository.WAQR_ACTION_EXTENSION );
-  		if ( bExists )
-  		{
-  			bOverwrite = window.confirm( Messages.getString( "overwriteFile", filename ) );
-  			
-  			if ( !bOverwrite )
-  			{
-  				return;
-  			}
-  		}
-  		else
-  		{
-  			bOverwrite = false;
-  		}
-  	}
-	
-		this.wiz.getBusyCtrl().setText( Messages.getString("saving") );
-		this.wiz.getBusyCtrl().show();
-		
-		this.solution = solution;
-		this.path = path;
-		this.filename = filename;
-		var outputType = this.repositoryBrowser.getOutputType();
-		if ( "" == outputType )
-		{
-		  outputType = "html,xls,pdf,csv";
-		}
-		this.reportSpec.reportName = filename;
-		
-		var templatesListCtrl = this.wiz.getPg( 0 ).getTemplatesListCtrl();
-		var items = templatesListCtrl.getSelectedItems();
-		var templatePath = items.length > 0
-			? items[0].itemData.templateFolderPath + "/" + items[0].itemData.jfreeTemplate
-			: "";
-		
-		var localThis = this;
-		this.bIsSaving = true;
-		var params = {
-			outputType:outputType,
-			templatePath: templatePath
-		};
-		
-		this.solutionRepository.save( solution, path, filename, this.reportSpec.asXml(),
-			params, bOverwrite, 
-			function( xmlDoc )
-			{
-				localThis.wiz.getBusyCtrl().hide();
-				if ( undefined != xmlDoc )
-				{
-					var msg = XmlUtil.getErrorMsg( xmlDoc );
-					if ( msg )
-					{
-						localThis.solution = null;
-						localThis.filename = null;
-						localThis.repositoryBrowser.msgCtrl.error( msg );
-					}
-					else
-					{
-						msg = XmlUtil.getStatusMsg( xmlDoc );
-						localThis.repositoryBrowser.msgCtrl.warn( msg );
-					}
-					localThis.bIsSaving = false;
-					
-					if ( localThis.onAfterSaveCallback )
-					{
-						localThis.onAfterSaveCallback()
-					}
-				}
-			}
-		);
-		return true;
-	}
-	else
-	{
-		this.wiz.msgCtrl.error( Messages.getString("invalidFileName", filename));
-		return false;
-	}
+
+  if ( !this.repositoryBrowser )
+  {
+    this.init();
+  }
+  
+  if ( RepositoryBrowserController.isValidFilename( filename ) )
+  {
+    /*
+     * if bOverwrite is false, check to see if the file exists in the client solutionRepository. If
+     * the file doesn't exist, we can safely set bOverwrite to false, that way if the file
+     * somehow gets created by another process before we save, the server will still
+     * detect it and return an error msg preventing an overwrite.
+     * If the file does exist, prompt the user for overwrite.
+     */
+    if ( !bOverwrite )
+    {
+      var bExists = this.solutionRepository.doesSolutionFileExist( solution, path, filename + SolutionRepository.WAQR_ACTION_EXTENSION );
+      if ( bExists )
+      {
+        bOverwrite = window.confirm( Messages.getString( "overwriteFile", filename ) );
+        
+        if ( !bOverwrite )
+        {
+          return;
+        }
+      }
+      else
+      {
+        bOverwrite = false;
+      }
+    }
+  
+    this.wiz.getBusyCtrl().setText( Messages.getString("saving") );
+    this.wiz.getBusyCtrl().show();
+    
+    this.solution = solution;
+    this.path = path;
+    this.filename = filename;
+    var outputType = this.getOutputType();
+        alert(outputType);
+    if ( "" == outputType )
+    {
+      outputType = "html,xls,pdf,csv";
+    }
+    this.reportSpec.reportName = filename;
+    
+    var templatesListCtrl = this.wiz.getPg( 0 ).getTemplatesListCtrl();
+    var items = templatesListCtrl.getSelectedItems();
+    var templatePath = items.length > 0
+      ? items[0].itemData.templateFolderPath + "/" + items[0].itemData.jfreeTemplate
+      : "";
+    
+    var localThis = this;
+    this.bIsSaving = true;
+    var params = {
+      outputType:outputType,
+      templatePath: templatePath
+    };
+    
+    this.solutionRepository.save( solution, path, filename, this.reportSpec.asXml(),
+      params, bOverwrite, 
+      function( xmlDoc )
+      {
+        localThis.wiz.getBusyCtrl().hide();
+        if ( undefined != xmlDoc )
+        {
+          var msg = XmlUtil.getErrorMsg( xmlDoc );
+          if ( msg )
+          {
+            localThis.solution = null;
+            localThis.filename = null;
+            localThis.repositoryBrowser.msgCtrl.error( msg );
+          }
+          else
+          {
+            msg = XmlUtil.getStatusMsg( xmlDoc );
+            localThis.repositoryBrowser.msgCtrl.warn( msg );
+          }
+          localThis.bIsSaving = false;
+          
+          if ( localThis.onAfterSaveCallback )
+          {
+            localThis.onAfterSaveCallback()
+          }
+        }
+      }
+    );
+    return true;
+  }
+  else
+  {
+    this.wiz.msgCtrl.error( Messages.getString("invalidFileName", filename));
+    return false;
+  }
 }
 
-	
+  
 RepositoryBrowserController.prototype.openSaveAsDialog = function()
 {
-	if ( !this.repositoryBrowser )
-	{
-		this.init();
-	}
-	this.repositoryBrowser.show();
+  if ( !this.repositoryBrowser )
+  {
+    this.init();
+  }
+  this.repositoryBrowser.show();
 }
+
 
 /**
  * @throws Status when the report spec is not valid and cannot be saved
  */
 RepositoryBrowserController.prototype.saveReportSpec = function()
 {
-	if ( StringUtils.isEmpty( this.solution ) 
-		|| StringUtils.isEmpty( this.path ) 
-		|| StringUtils.isEmpty( this.filename ) )
-	{
-		this.openSaveAsDialog()
-	}
-	else
-	{
-		// this.path better be non-empty too!
-		this.saveReportSpecAs( this.filename , this.solution, this.path, true );
-	}
+  if ( StringUtils.isEmpty( this.solution ) 
+    || StringUtils.isEmpty( this.path ) 
+    || StringUtils.isEmpty( this.filename ) )
+  {
+    this.openSaveAsDialog()
+  }
+  else
+  {
+    // this.path better be non-empty too!
+    this.saveReportSpecAs( this.filename , this.solution, this.path, true );
+  }
 }
 
 RepositoryBrowserController.prototype.reset = function()
 {
-	this.solution = null;
-	this.path = null;
-	this.filename = null;
-	this.reportSpec = null;
+  this.solution = null;
+  this.path = null;
+  this.filename = null;
+  this.reportSpec = null;
 }
 
 RepositoryBrowserController.prototype.setOnAfterSaveCallback = function( callback )
 {
-	this.onAfterSaveCallback = callback;
+  this.onAfterSaveCallback = callback;
 }
--- bi-platform-appserver/webapps/pentaho/adhoc/js/ui/WaqrWizard.js
+++ bi-platform-appserver/webapps/pentaho/adhoc/js/ui/WaqrWizard.js
@@ -17,11 +17,11 @@
 
 WaqrWizard = function( msgCtrl )
 {
-	this.msgCtrl = msgCtrl;
-	this.currPgNum = WaqrWizard.UNDEFINED_PAGE;
-	this.pgs = [ new WizPg0(), new WizPg1(), new WizPg2(), new WizPg3() ];
-	
-	Messages.setElementText("previewFormatPrompt", "previewFormatPrompt");
+  this.msgCtrl = msgCtrl;
+  this.currPgNum = WaqrWizard.UNDEFINED_PAGE;
+  this.pgs = [ new WizPg0(), new WizPg1(), new WizPg2(), new WizPg3() ];
+  
+  Messages.setElementText("previewFormatPrompt", "previewFormatPrompt");
 
     
     Messages.setElementText("step1_div", "step1_div");
@@ -29,59 +29,65 @@ WaqrWizard = function( msgCtrl )
     Messages.setElementText("step3_div", "step3_div");
     Messages.setElementText("step4_div", "step4_div");
     
-	var container = null;
-	var busyCtrl = new BusyCtrl();
-	container = document.getElementById( "busyContainer" );
-	container.appendChild( busyCtrl.getRoot() );
-	this.busyCtrl = busyCtrl;
-	
-	container = document.getElementById( "previewTypeContainer" );
-	var text = [ "HTML", "PDF", "Excel", "CSV" ];
-	var values = [ "html", "pdf", "xls", "csv" ];
-	var select = UIUtil.createSelectElem( text, values );
-	select.style.border = "1px solid #808080";
-	container.appendChild( select );
-	this.previewTypeSelect = select;
-	
-	container = document.getElementById( "previewContainer" );
-	var btn = new ButtonCtrl( Messages.getString("goBtnTxt"), ButtonCtrl.LARGE );
-	container.appendChild( btn.getRoot() );
-	this.previewBtn = btn;
-	
-	container = document.getElementById( "backContainer" );
-	btn = new ButtonCtrl( "&lt; " + Messages.getString("backBtnTxt"), ButtonCtrl.LARGE );
-	container.appendChild( btn.getRoot() );
-	this.backBtn = btn;
-	
-	container = document.getElementById( "nextContainer" );
-	btn = new ButtonCtrl( Messages.getString("nextBtnTxt") + " &gt;", ButtonCtrl.LARGE );
-	container.appendChild( btn.getRoot() );
-	this.nextBtn = btn;
-	
-	container = document.getElementById( "saveContainer" );
-	btn = new ButtonCtrl( Messages.getString("saveBtnTxt"), ButtonCtrl.LARGE );
-	container.appendChild( btn.getRoot() );
-	this.saveBtn = btn;
-	
-	container = document.getElementById( "saveAsContainer" );
-	btn = new ButtonCtrl( Messages.getString("saveAsBtnTxt"), ButtonCtrl.LARGE );
-	container.appendChild( btn.getRoot() );
-	this.saveAsBtn = btn;
-	
-	container = document.getElementById( "cancelContainer" );
-	btn = new ButtonCtrl( Messages.getString("cancelBtnTxt"), ButtonCtrl.LARGE );
-	if (!top.mantle_initialized) {
-		container.appendChild( btn.getRoot() );
-	}
-	this.cancelBtn = btn;
-	
-	var bEnable = false;
-	this.getNextBtn().setEnabled( bEnable );
-	this.getBackBtn().setEnabled( bEnable );
-	
-	this.getPreviewBtn().setEnabled( bEnable );
-	this.getSaveBtn().setEnabled( bEnable );
-	this.getSaveAsBtn().setEnabled( bEnable );
+  var container = null;
+  var busyCtrl = new BusyCtrl();
+  container = document.getElementById( "busyContainer" );
+  container.appendChild( busyCtrl.getRoot() );
+  this.busyCtrl = busyCtrl;
+  
+  container = document.getElementById( "previewTypeContainer" );
+  var text = [ "HTML", "PDF", "Excel", "CSV" ];
+  var values = [ "html", "pdf", "xls", "csv" ];
+  var select = UIUtil.createSelectElem( text, values );
+  select.style.border = "1px solid #808080";
+  container.appendChild( select );
+  this.previewTypeSelect = select;
+  
+  container = document.getElementById( "previewContainer" );
+  var btn = new ButtonCtrl( Messages.getString("goBtnTxt"), ButtonCtrl.LARGE );
+  container.appendChild( btn.getRoot() );
+  this.previewBtn = btn;
+  
+  container = document.getElementById( "backContainer" );
+  btn = new ButtonCtrl( "&lt; " + Messages.getString("backBtnTxt"), ButtonCtrl.LARGE );
+  container.appendChild( btn.getRoot() );
+  this.backBtn = btn;
+  
+  container = document.getElementById( "nextContainer" );
+  btn = new ButtonCtrl( Messages.getString("nextBtnTxt") + " &gt;", ButtonCtrl.LARGE );
+  container.appendChild( btn.getRoot() );
+  this.nextBtn = btn;
+  
+    var mantle_enabled = window.parent != null && window.parent.mantle_initialized == true;
+
+    container = document.getElementById( "saveContainer" );
+    btn = new ButtonCtrl( Messages.getString("saveBtnTxt"), ButtonCtrl.LARGE );
+     if(!mantle_enabled ){
+        container.appendChild( btn.getRoot() );
+    }
+    this.saveBtn = btn;
+    
+    container = document.getElementById( "saveAsContainer" );
+    btn = new ButtonCtrl( Messages.getString("saveAsBtnTxt"), ButtonCtrl.LARGE );
+     if(!mantle_enabled ){
+        container.appendChild( btn.getRoot() );
+    }
+    this.saveAsBtn = btn;
+
+  container = document.getElementById( "cancelContainer" );
+  btn = new ButtonCtrl( Messages.getString("cancelBtnTxt"), ButtonCtrl.LARGE );
+    if(!mantle_enabled ){
+    container.appendChild( btn.getRoot() );
+  }
+  this.cancelBtn = btn;
+  
+  var bEnable = false;
+  this.getNextBtn().setEnabled( bEnable );
+  this.getBackBtn().setEnabled( bEnable );
+  
+  this.getPreviewBtn().setEnabled( bEnable );
+  this.getSaveBtn().setEnabled( bEnable );
+  this.getSaveAsBtn().setEnabled( bEnable );
 };
 /*static*/WaqrWizard.UNDEFINED_PAGE = -1;
 // see AdhocWebService.NOT_SET_VALUE for server side instance of this value
@@ -89,63 +95,63 @@ WaqrWizard = function( msgCtrl )
 
 WaqrWizard.prototype.getPg = function( whichPg )
 {
-	return this.pgs[ whichPg ];
+  return this.pgs[ whichPg ];
 }
 WaqrWizard.prototype.showPg = function( pgNum )
 {
-	if ( (pgNum >= 0 ) && ( pgNum<WaqrWizard.NUM_PGS ) )
-	{
-		this.currPgNum = pgNum;
-		this.pgs[ pgNum ].showPg();
-	}
-	else
-	{
-		throw new Error( Messages.getString("invalidPageNum", pgNum));
-	}
+  if ( (pgNum >= 0 ) && ( pgNum<WaqrWizard.NUM_PGS ) )
+  {
+    this.currPgNum = pgNum;
+    this.pgs[ pgNum ].showPg();
+  }
+  else
+  {
+    throw new Error( Messages.getString("invalidPageNum", pgNum));
+  }
 }
 WaqrWizard.prototype.hidePg = function( pgNum )
 {
-	if ( (pgNum >= 0 ) && ( pgNum<WaqrWizard.NUM_PGS ) )
-	{
-		this.pgs[ pgNum ].hidePg();
-	}
-	else
-	{
-		throw new Error( Messages.getString("invalidPageNum", pgNum) );
-	}
+  if ( (pgNum >= 0 ) && ( pgNum<WaqrWizard.NUM_PGS ) )
+  {
+    this.pgs[ pgNum ].hidePg();
+  }
+  else
+  {
+    throw new Error( Messages.getString("invalidPageNum", pgNum) );
+  }
 }
 
 WaqrWizard.prototype.getBusyCtrl = function()
 {
-	return this.busyCtrl;
+  return this.busyCtrl;
 }
 WaqrWizard.prototype.getPreviewTypeSelect = function()
 {
-	return this.previewTypeSelect;
+  return this.previewTypeSelect;
 }
 WaqrWizard.prototype.getPreviewBtn = function()
 {
-	return this.previewBtn;
+  return this.previewBtn;
 }
 WaqrWizard.prototype.getBackBtn = function()
 {
-	return this.backBtn;
+  return this.backBtn;
 }
 WaqrWizard.prototype.getNextBtn = function()
 {
-	return this.nextBtn;
+  return this.nextBtn;
 }
 WaqrWizard.prototype.getSaveBtn = function()
 {
-	return this.saveBtn;
+  return this.saveBtn;
 }
 WaqrWizard.prototype.getSaveAsBtn = function()
 {
-	return this.saveAsBtn;
+  return this.saveAsBtn;
 }
 WaqrWizard.prototype.getCancelBtn = function()
 {
-	return this.cancelBtn;
+  return this.cancelBtn;
 }
 /* class static members: CN = className */
 WaqrWizard.NUM_PGS = 4;
