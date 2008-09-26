0ef483700e481aa9db993b0c139d521c98163459
dkincade <dkincade@5d404267-db13-0410-8476-b1f6b398e63a>
2008-09-26 19:05:13 +0000

    [BISERVER-1977] Added ability to reference the User Guide document from the Help -> Documentation menu item. If the application is running in "subscription mode", it will retrieve the EE version of the document. Otherwise it will return the CE version of the document.
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@32797 5d404267-db13-0410-8476-b1f6b398e63a

--- mantle/source/org/pentaho/mantle/client/commands/OpenDocCommand.java
+++ mantle/source/org/pentaho/mantle/client/commands/OpenDocCommand.java
@@ -1,5 +1,6 @@
 package org.pentaho.mantle.client.commands;
 
+import org.pentaho.gwt.widgets.client.dialogs.MessageDialogBox;
 import org.pentaho.mantle.client.messages.Messages;
 import org.pentaho.mantle.client.perspective.solutionbrowser.SolutionBrowserPerspective;
 import org.pentaho.mantle.login.client.MantleLoginService;
@@ -7,27 +8,26 @@ import org.pentaho.mantle.login.client.MantleLoginServiceAsync;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
+/**
+ * Executes the Open Document command. 
+ * @author nbaker / dkincade
+ */
 public class OpenDocCommand implements Command {
-
+  // Indicates if the instance running is a subscription instance 
   private static boolean isSubscription = false;
 
-  private static MantleLoginServiceAsync SERVICE;
   static {
-    SERVICE = (MantleLoginServiceAsync) GWT.create(MantleLoginService.class);
+    // Setup the service used to check if we are running in a subscription instance
+    final MantleLoginServiceAsync SERVICE = (MantleLoginServiceAsync) GWT.create(MantleLoginService.class);
     ServiceDefTarget endpoint = (ServiceDefTarget) SERVICE;
     String moduleRelativeURL = GWT.getModuleBaseURL() + "MantleLoginService"; //$NON-NLS-1$
     endpoint.setServiceEntryPoint(moduleRelativeURL);
-  }
 
-  SolutionBrowserPerspective navigatorPerspective;
-
-  public OpenDocCommand() {
+    // Check to see if the running instance is a subscription instance
     final AsyncCallback<Boolean> callback = new AsyncCallback<Boolean>() {
-
       public void onFailure(Throwable caught) {
         // Use the default - isSubscription = false;
       }
@@ -36,22 +36,24 @@ public class OpenDocCommand implements Command {
         isSubscription = result;
       }
     };
-
     SERVICE.isSubscription(callback);
   }
+
+  SolutionBrowserPerspective navigatorPerspective;
 
   public OpenDocCommand(SolutionBrowserPerspective navigatorPerspective) {
     this.navigatorPerspective = navigatorPerspective;
   }
 
+  /**
+   * Executes the command to open the help documentation. Based on the subscription setting,
+   * the document being opened will be the CE version of the document or the EE version 
+   * of the document.
+   */
   public void execute() {
     String documentation = Messages.getInstance().documentation();
     String documentationUrl = (isSubscription ? Messages.getInstance().documentationEEUrl() : Messages.getInstance()
         .documentationUrl());
-    if (!GWT.isScript()) {
-      documentationUrl = "http://localhost:8080/pentaho/" + documentationUrl;
-    }
     navigatorPerspective.showNewURLTab(documentation, documentationUrl, documentationUrl);
   }
-
 }
