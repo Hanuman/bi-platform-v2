5e4c20cf9b686861fb53117515e938618b43fc25
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2008-10-22 16:57:37 +0000

    fixed copyright date
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@33815 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-api/src/org/pentaho/platform/api/engine/IPentahoObjectFactory.java
+++ bi-platform-api/src/org/pentaho/platform/api/engine/IPentahoObjectFactory.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright 2005 - 2008 Pentaho Corporation.  All rights reserved.
+ * Copyright 2008 Pentaho Corporation.  All rights reserved.
  *
  * @created Oct 11, 2008
  * @author Aaron Phillips
