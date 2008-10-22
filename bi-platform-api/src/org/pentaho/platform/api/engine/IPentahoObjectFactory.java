38e34a3e853e9affcda4a2ce6bb909454b329363
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2008-10-22 13:57:31 +0000

    fixed license
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@33791 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-api/src/org/pentaho/platform/api/engine/IPentahoObjectFactory.java
+++ bi-platform-api/src/org/pentaho/platform/api/engine/IPentahoObjectFactory.java
@@ -1,25 +1,24 @@
 /*
  * This program is free software; you can redistribute it and/or modify it under the 
- * terms of the GNU General Public License, version 2 as published by the Free Software 
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software 
  * Foundation.
  *
- * You should have received a copy of the GNU General Public License along with this 
- * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html 
+ * You should have received a copy of the GNU Lesser General Public License along with this 
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html 
  * or from the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * See the GNU Lesser General Public License for more details.
  *
+ * Copyright 2005 - 2008 Pentaho Corporation.  All rights reserved.
  *
- * Copyright 2005 - 2008 Pentaho Corporation.  All rights reserved. 
- * 
  * @created Oct 11, 2008
  * @author Aaron Phillips
  * 
  */
-package org.pentaho.platform.api.engine;
+package org.pentaho.platform.api.engines;
 
 public interface IPentahoObjectFactory {
   /**
