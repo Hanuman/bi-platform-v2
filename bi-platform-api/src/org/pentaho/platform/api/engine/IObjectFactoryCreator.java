a5e6e14ad4be3f47586a0ab65a8f2453e9537f77
jdixon <jdixon@5d404267-db13-0410-8476-b1f6b398e63a>
2008-12-04 23:30:03 +0000

    PDB-102 Coding - use new factory API and implement class loading feature
    Removing the old factory classes
    
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@35052 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-api/src/org/pentaho/platform/api/engine/IObjectFactoryCreator.java
+++
@@ -1,8 +0,0 @@
-package org.pentaho.platform.api.engine;
-
-@Deprecated //IPentahoObjectFactory is no longer created via a factory
-public interface IObjectFactoryCreator {
-
-  public void configure( String configFilePath ) throws ObjectFactoryException;
-  public IPentahoObjectFactory getFactory();
-}
