cca7c8406fe30384e39e97471ea52049027f1f58
nbaker <nbaker@5d404267-db13-0410-8476-b1f6b398e63a>
2008-12-08 14:35:20 +0000

    Migrated re-usable loading code to helper class, AsyncXulLoader.
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@35134 5d404267-db13-0410-8476-b1f6b398e63a

--- mantle/source/org/pentaho/mantle/client/toolbars/XulMainToolbar.java
+++ mantle/source/org/pentaho/mantle/client/toolbars/XulMainToolbar.java
@@ -1,149 +1,92 @@
 package org.pentaho.mantle.client.toolbars;
 
 import org.pentaho.gwt.widgets.client.toolbar.Toolbar;
-import org.pentaho.gwt.widgets.client.utils.IMessageBundleLoadCallback;
 import org.pentaho.gwt.widgets.client.utils.MessageBundle;
-import org.pentaho.mantle.client.perspective.solutionbrowser.FileItem;
-import org.pentaho.mantle.client.perspective.solutionbrowser.IReloadableTabPanel;
-import org.pentaho.mantle.client.perspective.solutionbrowser.SolutionBrowserListener;
 import org.pentaho.mantle.client.perspective.solutionbrowser.SolutionBrowserPerspective;
 import org.pentaho.ui.xul.XulComponent;
+import org.pentaho.ui.xul.XulException;
 import org.pentaho.ui.xul.components.XulToolbarbutton;
 import org.pentaho.ui.xul.gwt.GwtXulDomContainer;
-import org.pentaho.ui.xul.gwt.GwtXulLoader;
 import org.pentaho.ui.xul.gwt.GwtXulRunner;
 import org.pentaho.ui.xul.gwt.tags.GwtToolbar;
 import org.pentaho.ui.xul.gwt.tags.GwtToolbarbutton;
+import org.pentaho.ui.xul.gwt.util.AsyncXulLoader;
 import org.pentaho.ui.xul.gwt.util.EventHandlerWrapper;
+import org.pentaho.ui.xul.gwt.util.IXulLoaderCallback;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.http.client.Request;
-import com.google.gwt.http.client.RequestBuilder;
-import com.google.gwt.http.client.RequestCallback;
-import com.google.gwt.http.client.RequestException;
-import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
-import com.google.gwt.xml.client.XMLParser;
 
-public class XulMainToolbar extends SimplePanel implements IMessageBundleLoadCallback, SolutionBrowserListener{
+public class XulMainToolbar extends SimplePanel implements IXulLoaderCallback{
   
-  private MessageBundle bundle;
-  
-  private SolutionBrowserPerspective solutionBrowser;
   
   private MainToolbarModel model;
   
   private MainToolbarController controller;
   
   public XulMainToolbar(final SolutionBrowserPerspective solutionBrowser){
-    this.solutionBrowser = solutionBrowser;
+    
+    //instantiate our Model and Controller
     model = new MainToolbarModel(solutionBrowser);
     controller = new MainToolbarController(model);
-    
     //TODO: remove controller reference from model when Bindings in place
     model.setController(controller);
     
+    // Invoke the async loading of the XUL DOM.
+    AsyncXulLoader.loadXul("xul/main_toolbar.xul", "messages/messages", this);
     
-    //Load the message bundle for the xul file.
-    try {
-      bundle = new MessageBundle("messages/","messages", this );    //$NON-NLS-1$   //$NON-NLS-2$
-    } catch (Exception e) {
-      Window.alert("Error loading message bundle: "+e.getMessage());    //$NON-NLS-1$
-      e.printStackTrace();
-    }
   }
   
   /**
-   * Called when the message bundle successfully loads. Here we request the XUL file from the sever
-   * and then hand it off for loading into the app.
+   * Callback method for the MantleXulLoader. This is called when the Xul file has been processed.
+   * 
+   * @param runner GwtXulRunner instance ready for event handlers and initializing.
    */
-  public void bundleLoaded(String bundleName) {
-    try {
-      RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, "xul/main_toolbar.xul");    //$NON-NLS-1$
-
-      try {
-        Request response = builder.sendRequest(null, new RequestCallback() {
-          public void onError(Request request, Throwable exception) {
-            Window.alert("Error loading XUL: "+exception.getMessage());   //$NON-NLS-1$
-          }
-
-          public void onResponseReceived(Request request, Response response) {
-            generateXul(response.getText());
-          }
-        });
-      } catch (RequestException e) {
-        Window.alert("error loading bundle: "+e.getMessage());    //$NON-NLS-1$
-      }
-    } catch (Exception e) {
-      Window.alert("error loading bundle: "+e.getMessage());    //$NON-NLS-1$
-      e.printStackTrace();
-    }
-  }
-
-  /**
-   * Takes the given XUL file, parses it and then grabs the Toolbar element for insertion into the 
-   * application.
-   */
-  private void generateXul(String xul) {
-    try {
-      
-      GwtXulLoader loader = new GwtXulLoader();
-      GwtXulRunner runner = new GwtXulRunner();
-
-      com.google.gwt.xml.client.Document gwtDoc = XMLParser.parse(xul);
-      GwtXulDomContainer container;
-
-      if(bundle != null){
-        container = loader.loadXul(gwtDoc, bundle);
-      } else {
-        container = loader.loadXul(gwtDoc);
-      }
-
-      
-      //handlers need to be wrapped genertically in GWT, create one and pass it our reference.
-      EventHandlerWrapper wrapper = GWT.create(MainToolbarController.class);
-      wrapper.setHandler(controller);
-
-      //
-      container.addEventHandler(wrapper);
-
-      runner.addContainer(container);
+  public void xulLoaded(GwtXulRunner runner)  {    
+    
+    // handlers need to be wrapped generically in GWT, create one and pass it our reference.
+    EventHandlerWrapper wrapper = GWT.create(MainToolbarController.class);
+    wrapper.setHandler(controller);
+  
+    // Add handler to container
+    GwtXulDomContainer container = (GwtXulDomContainer) runner.getXulDomContainers().get(0);
+    container.addEventHandler(wrapper);
+  
+    try{
       runner.initialize();
-
-      Toolbar bar = (Toolbar) container.getDocumentRoot().getElementById("mainToolbar").getManagedObject();    //$NON-NLS-1$
-      this.add(bar);
-      bar.setStylePrimaryName("mainToolbar");    //$NON-NLS-1$
+    } catch(XulException e){
+      Window.alert("Error initializing XUL runner: "+e.getMessage());    //$NON-NLS-1$
+      e.printStackTrace();
+      return;
+    }
+  
+    // Get the toolbar from the XUL doc
+    Toolbar bar = (Toolbar) container.getDocumentRoot().getElementById("mainToolbar").getManagedObject();    //$NON-NLS-1$
+    bar.setStylePrimaryName("mainToolbar");    //$NON-NLS-1$
+    this.add(bar);
+    
+    
+    //Fix for image locations in hosted mode. This really needs to be addressed Mantle-wide
+    if (!GWT.isScript()) {
       
-      //Fix for image locations in hosted mode. This really needs to be addressed
-      if (!GWT.isScript()) {
-        
-        GwtToolbar toolbar = (GwtToolbar) container.getDocumentRoot().getElementById("mainToolbar");
-        for(XulComponent c : toolbar.getChildNodes()){
-          if(c instanceof XulToolbarbutton){
-            GwtToolbarbutton btn = (GwtToolbarbutton) c;
-            
-            String curSrc = btn.getImage();
-            btn.setImage(curSrc.replace("mantle/", ""));
-            
-            curSrc = btn.getDisabledImage();
-            if(curSrc != null ){
-              btn.setDisabledImage(curSrc.replace("mantle/", ""));
-            }
+      GwtToolbar toolbar = (GwtToolbar) container.getDocumentRoot().getElementById("mainToolbar");  //$NON-NLS-1$
+      for(XulComponent c : toolbar.getChildNodes()){
+        if(c instanceof XulToolbarbutton){
+          GwtToolbarbutton btn = (GwtToolbarbutton) c;
+          
+          String curSrc = btn.getImage();
+          btn.setImage(curSrc.replace("mantle/", ""));    //$NON-NLS-1$ //$NON-NLS-2$
+          
+          curSrc = btn.getDisabledImage();
+          if(curSrc != null ){
+            btn.setDisabledImage(curSrc.replace("mantle/", ""));    //$NON-NLS-1$ //$NON-NLS-2$
           }
         }
       }
-
-      RootPanel.get().add(runner.getRootPanel());
-    } catch (Exception e) {
-      Window.alert("Error generating XUL: "+e.getMessage());    //$NON-NLS-1$
-      e.printStackTrace();
     }
   }
-  public void solutionBrowserEvent(IReloadableTabPanel panel, FileItem selectedFileItem) {
-      
-  }
+    
   
 }
 
