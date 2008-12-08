ea35431762d64a795ea1d1f110d9a2af7ab2e7d5
rmansoor <rmansoor@5d404267-db13-0410-8476-b1f6b398e63a>
2008-12-08 19:34:15 +0000

    PDB-97 - Making the new XUL -GWT toolbar behave like the current GWT toolbar for mantle
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@35146 5d404267-db13-0410-8476-b1f6b398e63a

--- mantle/source/org/pentaho/mantle/client/toolbars/MainToolbarController.java
+++ mantle/source/org/pentaho/mantle/client/toolbars/MainToolbarController.java
@@ -1,11 +1,9 @@
 package org.pentaho.mantle.client.toolbars;
 
-import org.pentaho.mantle.client.perspective.solutionbrowser.SolutionBrowserPerspective;
 import org.pentaho.ui.xul.EventMethod;
 import org.pentaho.ui.xul.components.XulButton;
+import org.pentaho.ui.xul.components.XulToolbarbutton;
 import org.pentaho.ui.xul.impl.AbstractXulEventHandler;
-
-import com.google.gwt.user.client.Window;
 
 /**
  * 
@@ -16,8 +14,15 @@ import com.google.gwt.user.client.Window;
 public class MainToolbarController extends AbstractXulEventHandler{
 
   private MainToolbarModel model;
-  private XulButton openBtn;
-  private XulButton saveBtn;
+  private XulToolbarbutton openBtn;
+  private XulToolbarbutton saveBtn;
+  private XulToolbarbutton saveAsBtn;
+  private XulToolbarbutton newAdhocBtn;
+  private XulToolbarbutton newAnalysisBtn;
+  private XulToolbarbutton printBtn;
+  private XulToolbarbutton workspaceBtn;
+  private XulToolbarbutton showBrowserBtn;
+  
   
   public MainToolbarController(MainToolbarModel model){
     this.model = model;
@@ -28,24 +33,87 @@ public class MainToolbarController extends AbstractXulEventHandler{
    */
   @EventMethod
   public void init(){
-    Window.alert("onLoad");
-    openBtn = (XulButton) document.getElementById("openButton");
-    saveBtn = (XulButton) document.getElementById("saveButton");
-    
+    openBtn = (XulToolbarbutton) document.getElementById("openButton");
+    saveBtn = (XulToolbarbutton) document.getElementById("saveButton");
+    saveAsBtn = (XulToolbarbutton) document.getElementById("saveAsButton");
+    newAdhocBtn = (XulToolbarbutton) document.getElementById("newAdhocButton");
+    newAnalysisBtn = (XulToolbarbutton) document.getElementById("newAnalysisButton");
+    printBtn = (XulToolbarbutton) document.getElementById("printButton");
+    workspaceBtn = (XulToolbarbutton) document.getElementById("workspaceButton");
+    showBrowserBtn = (XulToolbarbutton) document.getElementById("showBrowserButton");
   }
 
   @EventMethod
   public void openClicked(){
-    Window.alert("hello");
+    model.executeOpenFileCommand();
+  }
+
+  @EventMethod
+  public void newAnalysisClicked(){
+    model.executeAnalysisViewCommand();
   }
   
+  @EventMethod
+  public void newAdhocClicked(){
+    model.executeWAQRCommand();
+  }
+
+  @EventMethod
+  public void printClicked(){
+    model.executePrintCommand();
+  }
   
+  @EventMethod
+  public void saveClicked(){
+    model.executeSaveCommand();
+  }
+  
+  @EventMethod
+  public void saveAsClicked(){
+    model.executeSaveAsCommand();
+  }
+  
+  @EventMethod
+  public void workspaceClicked(){
+    model.executeWorkspaceCommand();
+    setWorkspaceSelected(model.isWorkspaceShowing());
+  }  
+  
+  @EventMethod
+  public void showBrowserClicked(){
+    model.executeShowBrowserCommand();
+    setShowBrowserSelected(model.isSolutionBrowserShowing());
+  }  
+  
+  public void setShowBrowserSelected(boolean flag) {
+    //called by the MainToolbarModel to change state.
+    showBrowserBtn.setSelected(flag);
+  }
+  
+  public void setWorkspaceSelected(boolean flag) {
+    //called by the MainToolbarModel to change state.
+    workspaceBtn.setSelected(flag);
+  }
   
   public void setSaveEnabled(boolean flag){
     //called by the MainToolbarModel to change state.
-    
+    saveBtn.setDisabled(!flag); 
   }
 
+  public void setSaveAsEnabled(boolean flag){
+    //called by the MainToolbarModel to change state.
+    saveAsBtn.setDisabled(!flag); 
+  }
+
+  public void setPrintEnabled(boolean flag){
+    //called by the MainToolbarModel to change state.
+    printBtn.setDisabled(!flag); 
+  }
+  
+  public void setNewAnalysisEnabled(boolean flag){
+    //called by the MainToolbarModel to change state.
+    newAnalysisBtn.setDisabled(!flag); 
+  }
   @Override
   public String getName() {
     return "mainToolbarHandler";
--- mantle/source/org/pentaho/mantle/client/toolbars/MainToolbarModel.java
+++ mantle/source/org/pentaho/mantle/client/toolbars/MainToolbarModel.java
@@ -1,5 +1,12 @@
 package org.pentaho.mantle.client.toolbars;
 
+import org.pentaho.mantle.client.commands.AnalysisViewCommand;
+import org.pentaho.mantle.client.commands.OpenFileCommand;
+import org.pentaho.mantle.client.commands.PrintCommand;
+import org.pentaho.mantle.client.commands.SaveCommand;
+import org.pentaho.mantle.client.commands.ShowBrowserCommand;
+import org.pentaho.mantle.client.commands.ToggleWorkspaceCommand;
+import org.pentaho.mantle.client.commands.WAQRCommand;
 import org.pentaho.mantle.client.perspective.solutionbrowser.FileItem;
 import org.pentaho.mantle.client.perspective.solutionbrowser.IReloadableTabPanel;
 import org.pentaho.mantle.client.perspective.solutionbrowser.SolutionBrowserListener;
@@ -16,6 +23,9 @@ public class MainToolbarModel implements SolutionBrowserListener{
   private SolutionBrowserPerspective solutionBrowser;
   
   private boolean saveEnabled = false;
+  private boolean saveAsEnabled = false;
+  private boolean printEnabled = false;
+  private boolean newAnalysisEnabled = false;
   
   //TODO: Remove once bindings in place
   private MainToolbarController controller;
@@ -23,7 +33,7 @@ public class MainToolbarModel implements SolutionBrowserListener{
   
   public MainToolbarModel(final SolutionBrowserPerspective solutionBrowser){
     this.solutionBrowser = solutionBrowser;
-    solutionBrowser.addSolutionBrowserListener(this);
+    this.solutionBrowser.addSolutionBrowserListener(this);
   }
   
   public void setSaveEnabled(Boolean enabled){
@@ -32,12 +42,86 @@ public class MainToolbarModel implements SolutionBrowserListener{
     //TODO: Replace following when bindings in place
     controller.setSaveEnabled(enabled);
   }
- 
+
+  public void setSaveAsEnabled(Boolean enabled){
+    saveAsEnabled = enabled;
+    
+    //TODO: Replace following when bindings in place
+    controller.setSaveAsEnabled(enabled);
+  }
+
+  public void setPrintEnabled(Boolean enabled){
+    printEnabled = enabled;
+    
+    //TODO: Replace following when bindings in place
+    controller.setPrintEnabled(enabled);
+  }
+  
+  public void setNewAnalysisEnabled(Boolean enabled){
+    newAnalysisEnabled = enabled;
+    
+    //TODO: Replace following when bindings in place
+    controller.setNewAnalysisEnabled(enabled);
+  }
+  
+  public void executeOpenFileCommand() {
+    OpenFileCommand openFileCommand = new OpenFileCommand(solutionBrowser);
+    openFileCommand.execute();
+  }
+  
+  public void executeAnalysisViewCommand() {
+    AnalysisViewCommand analysisViewCommand = new AnalysisViewCommand(solutionBrowser);
+    analysisViewCommand.execute();
+  }
+  
+  public void executePrintCommand() {
+    PrintCommand printCommand = new PrintCommand(solutionBrowser);
+    printCommand.execute();
+  }
+  
+  public void executeSaveCommand() {
+    SaveCommand saveCommand = new SaveCommand(solutionBrowser, false);
+    saveCommand.execute();
+  }  
+  
+  public void executeSaveAsCommand() {
+    SaveCommand saveCommand = new SaveCommand(solutionBrowser, true);
+    saveCommand.execute();
+  }  
+  
+  public void executeWAQRCommand() {
+    WAQRCommand wAQRCommand = new WAQRCommand(solutionBrowser);
+    wAQRCommand.execute();
+  }
+  
+  public void executeWorkspaceCommand() {
+    ToggleWorkspaceCommand toggleWorkspaceCommand = new ToggleWorkspaceCommand(solutionBrowser);
+    toggleWorkspaceCommand.execute();  
+  }
+  
+  public void executeShowBrowserCommand() {
+    ShowBrowserCommand showBrowserCommand = new ShowBrowserCommand(solutionBrowser);
+    showBrowserCommand.execute();
+  }
   /**
    * Process incoming events from the SolutionBrowser here
    */
   public void solutionBrowserEvent(IReloadableTabPanel panel, FileItem selectedFileItem) {
-    //copy logic from MainToolbar.java
+    String selectedTabURL = null;
+    boolean saveEnabled = false;
+
+    controller.setWorkspaceSelected(isWorkspaceShowing());
+    controller.setShowBrowserSelected(isSolutionBrowserShowing());
+    
+    if(panel != null){
+      selectedTabURL = panel.getUrl();
+      saveEnabled = panel.isSaveEnabled();
+    }
+    
+    setPrintEnabled(selectedTabURL != null && !"".equals(selectedTabURL)); //$NON-NLS-1$
+    setSaveEnabled(saveEnabled);
+    setSaveAsEnabled(saveEnabled);
+
   }
   
   public void setController(MainToolbarController controller){
@@ -45,6 +129,13 @@ public class MainToolbarModel implements SolutionBrowserListener{
     
   }
   
+  public boolean isSolutionBrowserShowing() {
+    return solutionBrowser.isNavigatorShowing();
+  }
+  
+  public boolean isWorkspaceShowing() {
+    return solutionBrowser.isWorkspaceShowing();
+  }
 }
 
   
