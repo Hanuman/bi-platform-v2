f5d5f8c1d511c04a710899eff5ef3a271adf1011
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2008-12-19 19:30:00 +0000

    added license and javadoc
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@35482 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-plugin-services/plugin-mgr/src/org/pentaho/platform/plugin/services/pluginmgr/SystemPathXmlPluginProvider.java
+++ bi-platform-plugin-services/plugin-mgr/src/org/pentaho/platform/plugin/services/pluginmgr/SystemPathXmlPluginProvider.java
@@ -1,3 +1,23 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software 
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this 
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html 
+ * or from the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright 2006 - 2008 Pentaho Corporation.  All rights reserved.
+ *
+ * Created Dec 19, 2008 
+ * @author aphillips
+ */
+
 package org.pentaho.platform.plugin.services.pluginmgr;
 
 import java.io.File;
@@ -30,6 +50,11 @@ import org.pentaho.ui.xul.IMenuCustomization.ItemType;
 import org.pentaho.ui.xul.impl.DefaultXulOverlay;
 import org.pentaho.ui.xul.util.MenuCustomization;
 
+/**
+ * An implmentation of {@link IPluginProvider} that searches for plugin.xml files in the Pentaho
+ * system path and instantiates {@link IPlatformPlugin}s from the information in those files.
+ * @author aphillips
+ */
 public class SystemPathXmlPluginProvider implements IPluginProvider {
 
   protected List<IPlatformPlugin> plugins = new ArrayList<IPlatformPlugin>();
