c5258a17598a66af319de6a33740faeb1f1198fd
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2009-01-02 21:17:18 +0000

    added license and some javadoc
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@35606 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-api/src/org/pentaho/platform/api/engine/IPlatformPlugin.java
+++ bi-platform-api/src/org/pentaho/platform/api/engine/IPlatformPlugin.java
@@ -1,9 +1,42 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software 
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this 
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html 
+ * or from the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright 2006 - 2008 Pentaho Corporation.  All rights reserved.
+ *
+ * Created Dec 19, 2008 
+ * @author aphillips
+ */
+
 package org.pentaho.platform.api.engine;
 
 import java.util.List;
 
 import org.pentaho.ui.xul.XulOverlay;
 
+/**
+ * The root of a logical grouping of BI Platform extensions.  Platform extension
+ * points include:
+ * <li>Content Generators  (@see {@link IContentGenerator})
+ * <li>Overlays  (@see {@link XulOverlay})
+ * <li>Menu Customizations
+ * 
+ * In addition to grouping extensions, an {@link IPlatformPlugin}
+ * include some attributes that allow the platform to identify and use the plugin
+ * appropriately.
+ * 
+ * @author jdixon
+ */
 public interface IPlatformPlugin {
 
   /**
