95975a57c5d5447372ec8f9bd93ac028cf452ee2
mlowery <mlowery@5d404267-db13-0410-8476-b1f6b398e63a>
2009-01-15 21:40:35 +0000

    [PPP-1527]
    * Removed dependency on PentahoSystem since this class is now instantiated from PAC.
    * Copied in encoding code since user password encoding is completely independent from datasource connection encoding.  (Note that they use the same Base64 encoding by default.)
    
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@35687 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-engine-security/src/org/pentaho/platform/engine/security/DefaultPentahoPasswordEncoder.java
+++ bi-platform-engine-security/src/org/pentaho/platform/engine/security/DefaultPentahoPasswordEncoder.java
@@ -19,22 +19,22 @@
 package org.pentaho.platform.engine.security;
 
 import org.acegisecurity.providers.encoding.PasswordEncoder;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.Validate;
-import org.pentaho.platform.api.engine.ObjectFactoryException;
-import org.pentaho.platform.api.util.IPasswordService;
-import org.pentaho.platform.api.util.PasswordServiceException;
-import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.security.messages.Messages;
+import org.pentaho.platform.util.StringUtil;
 import org.springframework.dao.DataAccessException;
 
 /**
  * Default password encoder for the BI Server.
  * 
- * <p>TODO Stop using <code>IPasswordService</code> since it is inherently reversible which is not a best practice.</p>
+ * <p>This encoder Base64-encodes the raw password.</p>
  * 
- * <p>Delegates to <code>IPasswordService</code>. Note that <code>IPasswordService</code> and 
- * <code>PasswordEncoder</code> are fundamentally different.</p> <code>IPasswordService</code> implements reversible 
- * encoding; <code>PasswordEncoder</code> is not reversible.
+ * <p>This class is instantiated by Pentaho Admin Console so there should not be a dependency on classes to which PAC
+ * will not have access.</p>
+ * 
+ * <p>This implementation of password encoding is completely independent of any datasource connection password 
+ * encoding.</p>
  * 
  * @author mlowery
  */
@@ -42,21 +42,8 @@ public class DefaultPentahoPasswordEncoder implements PasswordEncoder {
 
   public String encodePassword(final String rawPass, final Object salt) throws DataAccessException {
     Validate.notNull(rawPass, Messages.getString("DefaultPentahoPasswordEncoder.ERROR_0001_RAWPASS_CANNOT_BE_NULL")); //$NON-NLS-1$
-    IPasswordService passwordService = null;
-    try {
-      passwordService = (IPasswordService) PentahoSystem.getObjectFactory().getObject(
-          IPasswordService.IPASSWORD_SERVICE, null);
-    } catch (ObjectFactoryException e) {
-      throw new PasswordEncoderException(Messages
-          .getString("DefaultPentahoPasswordEncoder.ERROR_0003_PASSWORD_SERVICE_CANNOT_BE_CREATED"), e); //$NON-NLS-1$
-    }
-
-    try {
-      return passwordService.encrypt(rawPass);
-    } catch (PasswordServiceException e) {
-      throw new PasswordEncoderException(Messages
-          .getString("DefaultPentahoPasswordEncoder.ERROR_0004_PASSWORD_SERVICE_COULD_NOT_ENCRYPT"), e); //$NON-NLS-1$
-    }
+    // same code as org.pentaho.platform.util.Base64PasswordService.encrypt()
+    return StringUtil.isEmpty(rawPass) ? rawPass : new String(Base64.encodeBase64(rawPass.getBytes()));
   }
 
   public boolean isPasswordValid(final String encPass, final String rawPass, final Object salt)
@@ -67,17 +54,4 @@ public class DefaultPentahoPasswordEncoder implements PasswordEncoder {
     return encPass.equals(encodedRawPass);
   }
 
-  private class PasswordEncoderException extends DataAccessException {
-
-    private static final long serialVersionUID = -6706722564780658165L;
-
-    public PasswordEncoderException(final String msg, final Throwable cause) {
-      super(msg, cause);
-    }
-
-    public PasswordEncoderException(final String msg) {
-      super(msg);
-    }
-
-  }
 }
