bbda07789c54a71005e114f731a3d847555fc0e5
jdixon <jdixon@5d404267-db13-0410-8476-b1f6b398e63a>
2009-01-16 22:37:26 +0000

    Update unit tests
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@35709 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-engine-services/test-src/org/pentaho/test/platform/engine/services/MultiOutputTest.java
+++ bi-platform-engine-services/test-src/org/pentaho/test/platform/engine/services/MultiOutputTest.java
@@ -1,20 +1,14 @@
 package org.pentaho.test.platform.engine.services;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.pentaho.platform.api.engine.IRuntimeContext;
 import org.pentaho.platform.api.engine.ISolutionEngine;
-import org.pentaho.platform.engine.core.output.MultiContentItem;
-import org.pentaho.platform.engine.core.output.MultiOutputStream;
-import org.pentaho.platform.engine.core.output.SimpleContentItem;
 import org.pentaho.platform.util.web.SimpleUrlFactory;
 import org.pentaho.test.platform.engine.core.BaseTest;
-import org.pentaho.test.platform.engine.core.ExceptionOutputStream;
 
+@SuppressWarnings({"all"})
 public class MultiOutputTest extends BaseTest {
   private static final String SOLUTION_PATH = "test-src/solution";
 
@@ -27,120 +21,12 @@ public class MultiOutputTest extends BaseTest {
     startTest();
     ISolutionEngine solutionEngine = ServiceTestHelper.getSolutionEngine();
     String xactionStr = ServiceTestHelper.getXAction(SOLUTION_PATH, "services/MultiOutputTest.xaction");
-    PojoComponentTest.doneCalled = false;
     IRuntimeContext runtimeContext = solutionEngine
         .execute(
             xactionStr,
             "test1a.xaction", "empty action sequence test", false, true, null, false, new HashMap(), null, null, new SimpleUrlFactory(""), new ArrayList()); //$NON-NLS-1$ //$NON-NLS-2$
     finishTest();
 
-  }
-
-  public void testMultiContentItem() {
-
-    ByteArrayOutputStream out1 = new ByteArrayOutputStream();
-    ByteArrayOutputStream out2 = new ByteArrayOutputStream();
-
-    SimpleContentItem item1 = new SimpleContentItem(out1);
-    SimpleContentItem item2 = new SimpleContentItem(out2);
-
-    MultiContentItem multiContent = new MultiContentItem();
-    multiContent.addContentItem(item1);
-    multiContent.addContentItem(item2);
-
-    byte in[] = "abcd".getBytes();
-    String outStr1 = "";
-    String outStr2 = "";
-
-    try {
-      OutputStream multi = multiContent.getOutputStream("");
-      multi.write('a');
-      multi.write(in, 1, 2);
-      multi.write(in);
-      multi.close();
-    } catch (IOException e) {
-      // we should not get here
-      assertEquals("IOException", null, e);
-    }
-    outStr1 = new String(out1.toByteArray());
-    outStr2 = new String(out2.toByteArray());
-
-    assertEquals("Output stream 1", "abcabcd", outStr1);
-    assertEquals("Output stream 2", "abcabcd", outStr2);
-
-  }
-
-  public void testMultiStream() {
-
-    ByteArrayOutputStream out1 = new ByteArrayOutputStream();
-    ByteArrayOutputStream out2 = new ByteArrayOutputStream();
-
-    ByteArrayOutputStream outs[] = new ByteArrayOutputStream[] { out1, out2 };
-
-    MultiOutputStream multi = new MultiOutputStream(outs);
-    byte in[] = "abcd".getBytes();
-    String outStr1 = "";
-    String outStr2 = "";
-
-    try {
-      multi.write('a');
-      multi.write(in, 1, 2);
-      multi.write(in);
-      multi.close();
-    } catch (IOException e) {
-      // we should not get here
-      assertEquals("IOException", null, e);
-    }
-    outStr1 = new String(out1.toByteArray());
-    outStr2 = new String(out2.toByteArray());
-
-    assertEquals("Output stream 1", "abcabcd", outStr1);
-    assertEquals("Output stream 2", "abcabcd", outStr2);
-  }
-
-  public void testMultiStreamErrors() {
-
-    ByteArrayOutputStream out1 = new ByteArrayOutputStream();
-    ExceptionOutputStream out2 = new ExceptionOutputStream();
-    ByteArrayOutputStream out3 = new ByteArrayOutputStream();
-
-    OutputStream outs[] = new OutputStream[] { out1, out2, out3 };
-
-    MultiOutputStream multi = new MultiOutputStream(outs);
-    byte in[] = "abcd".getBytes();
-    String outStr1 = "";
-    String outStr2 = "";
-
-    try {
-      multi.write('a');
-    } catch (IOException e) {
-      // we expect to get here
-      assertEquals("IOException", "Test Exception", e.getMessage());
-    }
-    try {
-      multi.write(in, 1, 2);
-    } catch (IOException e) {
-      // we expect to get here
-      assertEquals("IOException", "Test Exception", e.getMessage());
-    }
-    try {
-      multi.write(in);
-    } catch (IOException e) {
-      // we expect to get here
-      assertEquals("IOException", "Test Exception", e.getMessage());
-    }
-    try {
-      multi.close();
-    } catch (IOException e) {
-      // we expect to get here
-      assertEquals("IOException", "Test Exception", e.getMessage());
-    }
-
-    outStr1 = new String(out1.toByteArray());
-    outStr2 = new String(out3.toByteArray());
-
-    assertEquals("Output stream 1", "abcabcd", outStr1);
-    assertEquals("Output stream 2", "abcabcd", outStr2);
   }
 
 }
--- bi-platform-engine-services/test-src/org/pentaho/test/platform/engine/services/SystemParameterProviderTest.java
+++ bi-platform-engine-services/test-src/org/pentaho/test/platform/engine/services/SystemParameterProviderTest.java
@@ -15,6 +15,7 @@ import org.pentaho.platform.engine.core.system.StandaloneSession;
 import org.pentaho.platform.util.web.SimpleUrlFactory;
 import org.pentaho.test.platform.engine.core.BaseTest;
 
+@SuppressWarnings({"all"})
 public class SystemParameterProviderTest extends BaseTest {
   private static final String SOLUTION_PATH = "test-src/solution";
 	  public String getSolutionPath() {
@@ -22,50 +23,18 @@ public class SystemParameterProviderTest extends BaseTest {
 	  }
 		public void testSystemParameter() {
 		    startTest();
-	        IPentahoSession session = new StandaloneSession("joe");
-	        ISolutionEngine solutionEngine = ServiceTestHelper.getSolutionEngine( session );
-	        try {
-	            String xactionStr = ServiceTestHelper.getXAction( SOLUTION_PATH, "services/params1.xaction" );
-	            Map<String,IParameterProvider> paramProviders = new HashMap<String,IParameterProvider>();
-	            SystemSettingsParameterProvider provider = new SystemSettingsParameterProvider();
-	            paramProviders.put( "system-settings" , provider);
-	            IRuntimeContext runtimeContext = solutionEngine.execute( 
-	            		xactionStr, "params1.xaction", "param test", false, true, null, false, paramProviders, null, null, new SimpleUrlFactory(""), new ArrayList()); //$NON-NLS-1$ //$NON-NLS-2$
-	            assertNotNull( "RuntimeContext is null", runtimeContext );
-	            assertEquals( "Action sequence execution failed", runtimeContext.getStatus(), IRuntimeContext.RUNTIME_STATUS_SUCCESS );
-	    	    IActionParameter param = runtimeContext.getOutputParameter( "output1" );
-	            assertNotNull( "Ouptut is null", param );
-	    	    assertEquals( "Output is not correct", "server.logserver.log", param.getStringValue() );
-	        } catch (Exception e) {
-	        	// we should not get here
-	        	e.printStackTrace();
-	        	assertTrue( e.getMessage(), false );
-	        }
+        SystemSettingsParameterProvider provider = new SystemSettingsParameterProvider();
+        assertEquals( "Output is not correct", "server.log", provider.getStringParameter("pentaho.xml{pentaho-system/log-file}", null) );
 	        finishTest();
 		}
 		
 		public void testCustomParameter() {
 		    startTest();
 	        IPentahoSession session = new StandaloneSession("joe");
-	        ISolutionEngine solutionEngine = ServiceTestHelper.getSolutionEngine( session );
-	        try {
-	            String xactionStr = ServiceTestHelper.getXAction( SOLUTION_PATH, "services/params2.xaction" );
-	            Map<String,IParameterProvider> paramProviders = new HashMap<String,IParameterProvider>();
-	            CustomSettingsParameterProvider provider = new CustomSettingsParameterProvider();
-	            provider.setSession( session );
-	            paramProviders.put( "custom-settings" , provider);
-	            IRuntimeContext runtimeContext = solutionEngine.execute( 
-	            		xactionStr, "params2.xaction", "param test", false, true, null, false, paramProviders, null, null, new SimpleUrlFactory(""), new ArrayList()); //$NON-NLS-1$ //$NON-NLS-2$
-	            assertNotNull( "RuntimeContext is null", runtimeContext );
-	            assertEquals( "Action sequence execution failed", runtimeContext.getStatus(), IRuntimeContext.RUNTIME_STATUS_SUCCESS );
-	    	    IActionParameter param = runtimeContext.getOutputParameter( "output1" );
-	            assertNotNull( "Ouptut is null", param );
-	    	    assertEquals( "Output is not correct", "value1value1", param.getStringValue() );
-	        } catch (Exception e) {
-	        	// we should not get here
-	        	e.printStackTrace();
-	        	assertTrue( e.getMessage(), false );
-	        }
+          CustomSettingsParameterProvider provider = new CustomSettingsParameterProvider();
+          provider.setSession( session );
+          
+          assertEquals( "Output is not correct", "value1", provider.getStringParameter("settings-{$user}.xml{personal-settings/setting1}", null ) );
 	        finishTest();
 		}
 }
