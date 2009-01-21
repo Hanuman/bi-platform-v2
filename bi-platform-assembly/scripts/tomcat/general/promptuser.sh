e6409e49a327b9b77f1bb7f0f22f1e55e9a9bf40
cboyden <cboyden@5d404267-db13-0410-8476-b1f6b398e63a>
2009-01-21 19:50:35 +0000

    [BISERVER-2702] Fixed URL per Jira
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@35805 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-assembly/scripts/tomcat/general/promptuser.sh
+++ bi-platform-assembly/scripts/tomcat/general/promptuser.sh
@@ -3,7 +3,7 @@ echo ---------------------------------------------------------------------------
 echo The Pentaho BI Platform now contains a version checker that will notify you
 echo when newer versions of the software are available. The version checker is enabled by default.
 echo For information on what the version checker does, why it is beneficial, and how it works see:
-echo http://wiki.pentaho.org/display/PentahoDoc/BI+Platform+Version+Checker
+echo http://wiki.pentaho.com/display/ServerDoc2x/Version+Checker
 echo Press Enter to continue, or type "cancel" or Ctrl-C to prevent the server from starting.
 echo You will only be prompted once with this question.
 echo --------------------------------------------------------------------------------------------
