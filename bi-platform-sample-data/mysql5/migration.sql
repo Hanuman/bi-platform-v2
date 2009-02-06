b878de5fb115b9376772aca4100bfda967f28251
rmansoor <rmansoor@5d404267-db13-0410-8476-b1f6b398e63a>
2009-02-06 16:16:47 +0000

    BISERVER-2596 - Updated the hbm xml to make username, password and query fields as optional also updated the migration.sql
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@36109 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-sample-data/mysql5/migration.sql
+++ bi-platform-sample-data/mysql5/migration.sql
@@ -1,4 +1,7 @@
 USE hibernate;
 -- Fixes a problem where the chronstring was not allowing nulls and couldn't be fixed by editing the mapping and reving the class
 ALTER TABLE PRO_SCHEDULE MODIFY CRONSTRING VARCHAR(256) NULL;
+ALTER TABLE DATASOURCE MODIFY USERNAME VARCHAR(50) NULL;
+ALTER TABLE DATASOURCE MODIFY PASSWORD VARCHAR(150) NULL;
+ALTER TABLE DATASOURCE MODIFY QUERY VARCHAR(100) NULL;
 commit;
