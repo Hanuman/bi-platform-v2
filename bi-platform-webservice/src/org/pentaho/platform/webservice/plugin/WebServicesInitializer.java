0ab8504cfc4c550c8f980276e2fabb2514177832
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2009-02-20 17:49:27 +0000

    [BISERVER-2803] - cleanup imports and remove old javadoc comment
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@36484 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-webservice/src/org/pentaho/platform/webservice/plugin/WebServicesInitializer.java
+++ bi-platform-webservice/src/org/pentaho/platform/webservice/plugin/WebServicesInitializer.java
@@ -1,20 +1,10 @@
 package org.pentaho.platform.webservice.plugin;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.api.engine.IPluginLifecycleListener;
 import org.pentaho.platform.api.engine.PluginLifecycleException;
 import org.pentaho.platform.plugin.services.webservices.AxisConfig;
 import org.pentaho.platform.plugin.services.webservices.IWebServiceConfigurator;
 
-/**
- * TODO BISERVER-2803 - this class acts as a plugin initializer.  We need to introduce plugin 
- * lifecycle support to the platform plugin framework and hook this in as an initializer.  This class is
- * currently configured as a content generator as a workaround to not having an initialize event.
- * It just so happens that content generators are test-loaded when the plugin is registered,
- * and this class depends on that behavior, however this is not actually a content generator at all.
- *   -ADP
- */
 public class WebServicesInitializer  implements IPluginLifecycleListener {
 
   private static final long serialVersionUID = 227084738820361822L;
