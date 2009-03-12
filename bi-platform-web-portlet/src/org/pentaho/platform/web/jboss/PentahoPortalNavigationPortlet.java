d50e2640c42d1a7d177c37a5ce5e82903dd41425
mbatchelor <mbatchelor@5d404267-db13-0410-8476-b1f6b398e63a>
2009-03-12 02:37:28 +0000

    Fix license headers.
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@36827 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-web-portlet/src/org/pentaho/platform/web/jboss/PentahoPortalNavigationPortlet.java
+++ bi-platform-web-portlet/src/org/pentaho/platform/web/jboss/PentahoPortalNavigationPortlet.java
@@ -1,17 +1,10 @@
 /*
- * Copyright 2007 Pentaho Corporation.  All rights reserved.
- * This software was developed by Pentaho Corporation and is provided under the terms
- * of the Mozilla Public License, Version 1.1, or any later version. You may not use
- * this file except in compliance with the license. If you need a copy of the license,
- * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho
- * BI Platform.  The Initial Developer is Pentaho Corporation.
- *
- * Software distributed under the Mozilla Public License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
- * the license for the specific language governing your rights and limitations.
- */
-/*
+ * Parts
+ * Copyright 2005 - 2009 Pentaho Corporation.  All rights reserved.
+ * 
+ * Parts
  * JBoss, Home of Professional Open Source
+ * 
  * Copyright 2005, JBoss Inc., and individual contributors as indicated
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
