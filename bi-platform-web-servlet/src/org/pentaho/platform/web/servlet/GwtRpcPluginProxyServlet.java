e5f0d211d49bebf44bffea4677f8841e2faef7e4
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2009-05-28 23:04:21 +0000

    put debug in gwtrpc proxy to figure out why it can't load resources
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@37809 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-web-servlet/src/org/pentaho/platform/web/servlet/GwtRpcPluginProxyServlet.java
+++ bi-platform-web-servlet/src/org/pentaho/platform/web/servlet/GwtRpcPluginProxyServlet.java
@@ -20,9 +20,16 @@
 
 package org.pentaho.platform.web.servlet;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.pentaho.platform.api.engine.IPluginManager;
+import org.pentaho.platform.api.engine.IPluginResourceLoader;
 import org.pentaho.platform.api.engine.PluginBeanException;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 
@@ -31,6 +38,8 @@ import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.server.rpc.RPC;
 import com.google.gwt.user.server.rpc.RPCRequest;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.google.gwt.user.server.rpc.SerializationPolicy;
+import com.google.gwt.user.server.rpc.SerializationPolicyLoader;
 
 public class GwtRpcPluginProxyServlet extends RemoteServiceServlet {
 
@@ -53,6 +62,7 @@ public class GwtRpcPluginProxyServlet extends RemoteServiceServlet {
     return requestPathInfo;
   }
 
+  @Override
   public String processCall(String payload) throws SerializationException {
     IPluginManager pluginManager = PentahoSystem.get(IPluginManager.class, null);
     
@@ -91,5 +101,92 @@ public class GwtRpcPluginProxyServlet extends RemoteServiceServlet {
         Thread.currentThread().setContextClassLoader(origLoader);
       }
     }
+  }
+  
+  @Override
+  protected SerializationPolicy doGetSerializationPolicy(
+      HttpServletRequest request, String moduleBaseURL, String strongName) {
+    
+    
+    // The request can tell you the path of the web app relative to the
+    // container root.
+    String contextPath = request.getContextPath();
+
+    String modulePath = null;
+    if (moduleBaseURL != null) {
+      try {
+        modulePath = new URL(moduleBaseURL).getPath();
+      } catch (MalformedURLException ex) {
+        // log the information, we will default
+        log("Malformed moduleBaseURL: " + moduleBaseURL, ex);
+      }
+    }
+
+    SerializationPolicy serializationPolicy = null;
+
+    /*
+     * Check that the module path must be in the same web app as the servlet
+     * itself. If you need to implement a scheme different than this, override
+     * this method.
+     */
+    if (modulePath == null || !modulePath.startsWith(contextPath)) {
+      String message = "ERROR: The module path requested, "
+          + modulePath
+          + ", is not in the same web application as this servlet, "
+          + contextPath
+          + ".  Your module may not be properly configured or your client and server code maybe out of date.";
+      log(message, null);
+    } else {
+      // Strip off the context path from the module base URL. It should be a
+      // strict prefix.
+      String contextRelativePath = modulePath.substring(contextPath.length());
+
+      String serializationPolicyFilePath = SerializationPolicyLoader.getSerializationPolicyFileName(contextRelativePath
+          + strongName);
+
+      // Open the RPC resource file read its contents.
+      InputStream is = getServletContext().getResourceAsStream(serializationPolicyFilePath);
+      if(is == null) {
+        log(serializationPolicyFilePath + " didn't work");
+        is = getServletContext().getResourceAsStream(".."+serializationPolicyFilePath);
+      }
+      if(is == null) {
+        log(".."+serializationPolicyFilePath + " didn't work");
+        is = getServletContext().getResourceAsStream(serializationPolicyFilePath.substring(1));
+      }
+      if(is == null) {
+        log(serializationPolicyFilePath.substring(1) + " didn't work, giving up");
+        is = getServletContext().getResourceAsStream(contextPath+serializationPolicyFilePath.substring(1));
+      }
+      try {
+        if (is != null) {
+          try {
+            serializationPolicy = SerializationPolicyLoader.loadFromStream(is,
+                null);
+          } catch (ParseException e) {
+            log("ERROR: Failed to parse the policy file '"
+                + serializationPolicyFilePath + "'", e);
+          } catch (IOException e) {
+            log("ERROR: Could not read the policy file '"
+                + serializationPolicyFilePath + "'", e);
+          }
+        } else {
+          String message = "ERROR: The serialization policy file '"
+              + serializationPolicyFilePath
+              + "' was not found; did you forget to include it in this deployment?";
+          log(message, null);
+        }
+      } finally {
+        if (is != null) {
+          try {
+            is.close();
+          } catch (IOException e) {
+            // Ignore this error
+          }
+        }
+      }
+    }
+
+    return serializationPolicy;
   }
 }
