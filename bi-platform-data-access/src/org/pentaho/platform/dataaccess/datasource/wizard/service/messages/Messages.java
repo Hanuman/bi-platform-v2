6b310887f2da97bf413d91d4ae50ff0e90659251
rmansoor <rmansoor@5d404267-db13-0410-8476-b1f6b398e63a>
2009-06-03 14:19:32 +0000

    Checking in fix for model not being saved
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@37883 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-data-access/src/org/pentaho/platform/dataaccess/datasource/wizard/service/messages/Messages.java
+++ bi-platform-data-access/src/org/pentaho/platform/dataaccess/datasource/wizard/service/messages/Messages.java
@@ -33,7 +33,7 @@ import org.pentaho.platform.util.messages.MessageUtil;
 
 
 public class Messages {
-    private static final String BUNDLE_NAME = Messages.class.getPackage().getName() + ".messages"; //$NON-NLS-1$
+  private static final String BUNDLE_NAME = "org.pentaho.platform.dataaccess.datasource.wizard.service.messages"; //$NON-NLS-1$
 
   private static final Map locales = Collections.synchronizedMap(new HashMap());
 
