908c7bb3b644b19ff06a4a9b4710f917e16b04ef
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2009-06-11 15:47:26 +0000

    * removed old pure GWT RPC servlets *GwtServlet classes * cleaned out old comments in the *ServiceBeanImpl classes * plugin now exposes a connection web serivce
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@38019 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-data-access/src/org/pentaho/platform/dataaccess/datasource/wizard/service/gwt/ConnectionServiceBeanImpl.java
+++ bi-platform-data-access/src/org/pentaho/platform/dataaccess/datasource/wizard/service/gwt/ConnectionServiceBeanImpl.java
@@ -20,7 +20,7 @@ public class ConnectionServiceBeanImpl implements ConnectionGwtService {
       try {
         IPentahoSession session = PentahoSessionHolder.getSession();
         
-        datasourceMgmtSvc = PentahoSystem.get(IDatasourceMgmtService.class, session); //$NON-NLS-1$
+        datasourceMgmtSvc = PentahoSystem.get(IDatasourceMgmtService.class, session);
         SERVICE = new ConnectionServiceDelegate(datasourceMgmtSvc);
         
       } catch (Exception e) {
@@ -56,86 +56,4 @@ public class ConnectionServiceBeanImpl implements ConnectionGwtService {
   public Boolean testConnection(IConnection connection) throws ConnectionServiceException {
     return getService().testConnection(connection);    
   }
-  
-//  @Override
-//  protected SerializationPolicy doGetSerializationPolicy(HttpServletRequest arg0, String arg1, String arg2) {
-//      return new SerializationPolicy(){
-//
-//        List<Class<?>> classes = new ArrayList<Class<?>>();
-//        {
-//          classes.add(Exception.class);
-//          classes.add(Integer.class);
-//          classes.add(Number.class);
-//          classes.add(Boolean.class);
-//          classes.add(RuntimeException.class);
-//          classes.add(String.class);
-//          classes.add(Throwable.class);
-//          classes.add(ArrayList.class);
-//          classes.add(HashMap.class);
-//          classes.add(LinkedHashMap.class);
-//          classes.add(LinkedList.class);
-//          classes.add(Stack.class);
-//          classes.add(Vector.class);
-//          classes.add(MqlDomain.class);
-//          classes.add(MqlColumn.class);
-//          classes.add(MqlCondition.class);
-//          classes.add(MqlOrder.class);
-//          classes.add(ColumnType.class);
-//          classes.add(CombinationType.class);
-//          classes.add(MqlBusinessTable.class);
-//          classes.add(MqlCategory.class);
-//          classes.add(MqlModel.class);
-//          classes.add(MqlQuery.class);
-//          classes.add(Operator.class);
-//          classes.add(DatabaseColumnType.class);
-//          classes.add(Connection.class);
-//          classes.add(Datasource.class);
-//          classes.add(MqlOrder.class);
-//          classes.add(Column.class);
-//          classes.add(Attribute.class);
-//          classes.add(Envelope.class);
-//          classes.add(BusinessData.class);
-//          classes.add(SerializedResultSet.class);
-//          classes.add(DataType.class);
-//          classes.add(Domain.class);
-//          classes.add(IPhysicalColumn.class);
-//          classes.add(LogicalModel.class);
-//          classes.add(Category.class);
-//          classes.add(LogicalColumn.class);
-//          classes.add(SqlPhysicalColumn.class);
-//          classes.add(SqlPhysicalModel.class);
-//          classes.add(SqlPhysicalTable.class);
-//          classes.add(Concept.class);
-//          classes.add(AggregationType.class);
-//          classes.add(DataType.class);
-//          classes.add(TargetColumnType.class);
-//          classes.add(TargetTableType.class);
-//          classes.add(LocalizedString.class);
-//        }
-//        @Override
-//        public boolean shouldDeserializeFields(Class<?> clazz) {
-//          return classes.contains(clazz);
-//        }
-//
-//        @Override
-//        public boolean shouldSerializeFields(Class<?> clazz) {
-//
-//          return classes.contains(clazz);
-//            
-//        }
-//
-//        @Override
-//        public void validateDeserialize(Class<?> arg0) throws SerializationException {
-//          
-//            
-//        }
-//
-//        @Override
-//        public void validateSerialize(Class<?> arg0) throws SerializationException {
-//          
-//            
-//        }
-//        
-//      };
-
 }
--- bi-platform-data-access/src/org/pentaho/platform/dataaccess/datasource/wizard/service/gwt/DatasourceServiceBeanImpl.java
+++ bi-platform-data-access/src/org/pentaho/platform/dataaccess/datasource/wizard/service/gwt/DatasourceServiceBeanImpl.java
@@ -11,7 +11,6 @@ import org.pentaho.platform.dataaccess.datasource.utils.SerializedResultSet;
 import org.pentaho.platform.dataaccess.datasource.wizard.service.DatasourceServiceException;
 import org.pentaho.platform.dataaccess.datasource.wizard.service.impl.DatasourceServiceDelegate;
 import org.pentaho.platform.plugin.services.webservices.PentahoSessionHolder;
-import org.pentaho.platform.web.http.PentahoHttpSessionHelper;
 
 public class DatasourceServiceBeanImpl implements DatasourceGwtService {
 
@@ -70,87 +69,6 @@ public class DatasourceServiceBeanImpl implements DatasourceGwtService {
     SERVICE.setSession(PentahoSessionHolder.getSession());
     return SERVICE.isAdministrator();
   }
-
-//  @Override
-//  protected SerializationPolicy doGetSerializationPolicy(HttpServletRequest arg0, String arg1, String arg2) {
-//    return new SerializationPolicy() {
-//
-//      List<Class<?>> classes = new ArrayList<Class<?>>();
-//      {
-//        classes.add(Exception.class);
-//        classes.add(Integer.class);
-//        classes.add(Number.class);
-//        classes.add(Boolean.class);
-//        classes.add(RuntimeException.class);
-//        classes.add(String.class);
-//        classes.add(Throwable.class);
-//        classes.add(ArrayList.class);
-//        classes.add(HashMap.class);
-//        classes.add(LinkedHashMap.class);
-//        classes.add(LinkedList.class);
-//        classes.add(Stack.class);
-//        classes.add(Vector.class);
-//        classes.add(MqlDomain.class);
-//        classes.add(MqlColumn.class);
-//        classes.add(MqlCondition.class);
-//        classes.add(MqlOrder.class);
-//        classes.add(ColumnType.class);
-//        classes.add(CombinationType.class);
-//        classes.add(MqlBusinessTable.class);
-//        classes.add(MqlCategory.class);
-//        classes.add(MqlModel.class);
-//        classes.add(MqlQuery.class);
-//        classes.add(Operator.class);
-//        classes.add(DatabaseColumnType.class);
-//        classes.add(Connection.class);
-//        classes.add(Datasource.class);
-//        classes.add(MqlOrder.class);
-//        classes.add(Column.class);
-//        classes.add(Attribute.class);
-//        classes.add(Envelope.class);
-//        classes.add(BusinessData.class);
-//        classes.add(SerializedResultSet.class);
-//        classes.add(DataType.class);
-//        classes.add(Domain.class);
-//        classes.add(IPhysicalColumn.class);
-//        classes.add(LogicalModel.class);
-//        classes.add(Category.class);
-//        classes.add(LogicalColumn.class);
-//        classes.add(SqlPhysicalColumn.class);
-//        classes.add(SqlPhysicalModel.class);
-//        classes.add(SqlPhysicalTable.class);
-//        classes.add(Concept.class);
-//        classes.add(AggregationType.class);
-//        classes.add(DataType.class);
-//        classes.add(TargetColumnType.class);
-//        classes.add(TargetTableType.class);
-//        classes.add(LocalizedString.class);
-//      }
-//
-//      @Override
-//      public boolean shouldDeserializeFields(Class<?> clazz) {
-//        return classes.contains(clazz);
-//      }
-//
-//      @Override
-//      public boolean shouldSerializeFields(Class<?> clazz) {
-//
-//        return classes.contains(clazz);
-//
-//      }
-//
-//      @Override
-//      public void validateDeserialize(Class<?> arg0) throws SerializationException {
-//
-//      }
-//
-//      @Override
-//      public void validateSerialize(Class<?> arg0) throws SerializationException {
-//
-//      }
-//
-//    };
-//  }
 
   public BogoPojo gwtWorkaround(BogoPojo pojo) {
     return pojo;
