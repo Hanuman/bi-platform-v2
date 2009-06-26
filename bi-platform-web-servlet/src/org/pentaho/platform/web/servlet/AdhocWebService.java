a70770f76c3915b93a5def2e0b3c4154fb9850d5
rmansoor <rmansoor@5d404267-db13-0410-8476-b1f6b398e63a>
2009-06-26 15:07:35 +0000

    Using the datasource service to check if the user has access to create or delete datasources
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@38220 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-web-servlet/src/org/pentaho/platform/web/servlet/AdhocWebService.java
+++ bi-platform-web-servlet/src/org/pentaho/platform/web/servlet/AdhocWebService.java
@@ -322,8 +322,6 @@ public class AdhocWebService extends ServletBase {
       deleteWaqrReport(parameterProvider, outputStream, userSession, wrapWithSoap);
     } else if ("getJFreePaperSizes".equals(component)) { //$NON-NLS-1$
       getJFreePaperSizes(parameterProvider, outputStream, userSession, wrapWithSoap);
-    } else if ("isAdministrator".equals(component)) { //$NON-NLS-1$
-      isAdministrator(outputStream, userSession, wrapWithSoap);
     } else {
       throw new RuntimeException(Messages.getErrorString("HttpWebService.UNRECOGNIZED_COMPONENT_REQUEST", component)); //$NON-NLS-1$
     }
@@ -1932,16 +1930,6 @@ public class AdhocWebService extends ServletBase {
     return baseName;
   }
 
-  public void isAdministrator(OutputStream outputStream, final IPentahoSession userSession, final boolean wrapWithSoap) {
-    
-    try {
-      WebServiceUtil.writeDocument(outputStream, buildResponse(SecurityHelper.isPentahoAdministrator(userSession)), wrapWithSoap);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
-  
   // should be moved to AdhocWebServiceTest, but the solvable problem of calling
   // private methods needs to be addressed. don't have time at the moment, dude.
   // to run this, you need to have -ea on the JVM command line
@@ -1983,14 +1971,6 @@ public class AdhocWebService extends ServletBase {
     
     System.out.println( "looks like it worked." );
 
-  }
-  private Document buildResponse(boolean value) {
-    Document document = DocumentHelper.createDocument();
-    Element root = document.addElement("adhoc-webservice");
-    Element isAdminElement = DocumentHelper.createElement("isadmin");
-    isAdminElement.addAttribute("value", Boolean.toString(value));
-    root.add(isAdminElement);
-    return document;
   }
 
 }
