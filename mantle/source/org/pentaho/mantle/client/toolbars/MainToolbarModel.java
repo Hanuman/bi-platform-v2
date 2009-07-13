e49ea9c6041efee6bf93884532a49c5680e259ed
mdamour <mdamour@5d404267-db13-0410-8476-b1f6b398e63a>
2009-07-13 13:48:06 +0000

    Code reformatting.
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@38450 5d404267-db13-0410-8476-b1f6b398e63a

--- mantle/source/org/pentaho/mantle/client/toolbars/MainToolbarModel.java
+++ mantle/source/org/pentaho/mantle/client/toolbars/MainToolbarModel.java
@@ -14,7 +14,7 @@
  *
  * Copyright 2008 - 2009 Pentaho Corporation.  All rights reserved.
  * 
-*/
+ */
 package org.pentaho.mantle.client.toolbars;
 
 import org.pentaho.mantle.client.XulMain;
@@ -32,12 +32,13 @@ import org.pentaho.ui.xul.XulEventSourceAdapter;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
- * State model for the main toolbar. Replace controller code calls with Bindings when available.
+ * State model for the main toolbar. Replace controller code calls with Bindings
+ * when available.
  * 
  * @author NBaker
  */
-public class MainToolbarModel extends XulEventSourceAdapter implements SolutionBrowserListener
-{
+public class MainToolbarModel extends XulEventSourceAdapter implements
+    SolutionBrowserListener {
 
   private SolutionBrowserPerspective solutionBrowser;
   private XulMain main;
@@ -50,81 +51,71 @@ public class MainToolbarModel extends XulEventSourceAdapter implements SolutionB
   private boolean showBrowserSelected = false;
   private boolean workspaceSelected = false;
 
-  public MainToolbarModel(final SolutionBrowserPerspective solutionBrowser, XulMain main)
-  {
+  public MainToolbarModel(final SolutionBrowserPerspective solutionBrowser,
+      XulMain main) {
     this.solutionBrowser = solutionBrowser;
     this.solutionBrowser.addSolutionBrowserListener(this);
     this.main = main;
   }
 
-  public void setSaveEnabled(Boolean enabled)
-  {
+  public void setSaveEnabled(Boolean enabled) {
     boolean prevVal = this.saveEnabled;
     saveEnabled = enabled;
     this.firePropertyChange("saveEnabled", prevVal, saveEnabled);
   }
 
-  public boolean isSaveEnabled()
-  {
+  public boolean isSaveEnabled() {
     return this.saveEnabled;
   }
 
-  public void setSaveAsEnabled(Boolean enabled)
-  {
+  public void setSaveAsEnabled(Boolean enabled) {
     boolean prevVal = this.saveAsEnabled;
     saveAsEnabled = enabled;
 
     this.firePropertyChange("saveAsEnabled", prevVal, saveAsEnabled);
   }
 
-  public void setPrintEnabled(Boolean enabled)
-  {
+  public void setPrintEnabled(Boolean enabled) {
     boolean prevVal = this.printEnabled;
     printEnabled = enabled;
 
     this.firePropertyChange("printEnabled", prevVal, enabled);
   }
 
-  public void setNewAnalysisEnabled(Boolean enabled)
-  {
+  public void setNewAnalysisEnabled(Boolean enabled) {
     boolean prevVal = this.newAnalysisEnabled;
     newAnalysisEnabled = enabled;
 
     this.firePropertyChange("newAnalysisEnabled", prevVal, newAnalysisEnabled);
   }
 
-  public void executeOpenFileCommand()
-  {
+  public void executeOpenFileCommand() {
     OpenFileCommand openFileCommand = new OpenFileCommand(solutionBrowser);
     openFileCommand.execute();
   }
 
-  public void executeAnalysisViewCommand()
-  {
-    AnalysisViewCommand analysisViewCommand = new AnalysisViewCommand(solutionBrowser);
+  public void executeAnalysisViewCommand() {
+    AnalysisViewCommand analysisViewCommand = new AnalysisViewCommand(
+        solutionBrowser);
     analysisViewCommand.execute();
   }
 
-  public void executePrintCommand()
-  {
+  public void executePrintCommand() {
     PrintCommand printCommand = new PrintCommand(solutionBrowser);
     printCommand.execute();
   }
 
-  public void executeSaveCommand()
-  {
+  public void executeSaveCommand() {
     SaveCommand saveCommand = new SaveCommand(solutionBrowser, false);
     saveCommand.execute();
   }
 
-  public void executeSaveAsCommand()
-  {
+  public void executeSaveAsCommand() {
     SaveCommand saveCommand = new SaveCommand(solutionBrowser, true);
     saveCommand.execute();
   }
 
-  public void executeWAQRCommand()
-  {
+  public void executeWAQRCommand() {
     WAQRCommand wAQRCommand = new WAQRCommand(solutionBrowser);
     wAQRCommand.execute();
   }
@@ -132,15 +123,14 @@ public class MainToolbarModel extends XulEventSourceAdapter implements SolutionB
   /**
    * Process incoming events from the SolutionBrowser here
    */
-  public void solutionBrowserEvent(SolutionBrowserListener.EventType type, Widget panel, FileItem selectedFileItem)
-  {
+  public void solutionBrowserEvent(SolutionBrowserListener.EventType type,
+      Widget panel, FileItem selectedFileItem) {
     String selectedTabURL = null;
     boolean saveEnabled = false;
     boolean editIsEnabled = false;
     boolean editSelected = false;
 
-    if (panel != null && panel instanceof ReloadableIFrameTabPanel)
-    {
+    if (panel != null && panel instanceof ReloadableIFrameTabPanel) {
       selectedTabURL = ((ReloadableIFrameTabPanel) panel).getUrl();
       saveEnabled = ((ReloadableIFrameTabPanel) panel).isSaveEnabled();
       editIsEnabled = ((ReloadableIFrameTabPanel) panel).isEditEnabled();
@@ -156,77 +146,68 @@ public class MainToolbarModel extends XulEventSourceAdapter implements SolutionB
     setWorkspaceSelected(solutionBrowser.isWorkspaceShowing());
     setShowBrowserSelected(solutionBrowser.isNavigatorShowing());
 
-    if (panel instanceof ReloadableIFrameTabPanel)
-    {
-      if (SolutionBrowserListener.EventType.OPEN.equals(type) || SolutionBrowserListener.EventType.SELECT.equals(type))
-      {
-        if (panel != null)
-        {
-          main.applyOverlays(((ReloadableIFrameTabPanel) panel).getOverlayIds());
+    if (panel instanceof ReloadableIFrameTabPanel) {
+      if (SolutionBrowserListener.EventType.OPEN.equals(type)
+          || SolutionBrowserListener.EventType.SELECT.equals(type)) {
+        if (panel != null) {
+          main
+              .applyOverlays(((ReloadableIFrameTabPanel) panel).getOverlayIds());
         }
-      }
-      else if (SolutionBrowserListener.EventType.CLOSE.equals(type) || SolutionBrowserListener.EventType.DESELECT.equals(type))
-      {
-        if (panel != null)
-        {
-          main.removeOverlays(((ReloadableIFrameTabPanel) panel).getOverlayIds());
+      } else if (SolutionBrowserListener.EventType.CLOSE.equals(type)
+          || SolutionBrowserListener.EventType.DESELECT.equals(type)) {
+        if (panel != null) {
+          main.removeOverlays(((ReloadableIFrameTabPanel) panel)
+              .getOverlayIds());
         }
       }
     }
   }
 
-  public boolean isShowBrowserSelected()
-  {
+  public boolean isShowBrowserSelected() {
     return showBrowserSelected;
   }
 
-  public boolean isWorkspaceSelected()
-  {
+  public boolean isWorkspaceSelected() {
     return workspaceSelected;
   }
 
-  public void setShowBrowserSelected(boolean showBrowserSelected)
-  {
+  public void setShowBrowserSelected(boolean showBrowserSelected) {
     boolean prevVal = this.showBrowserSelected;
 
     this.showBrowserSelected = showBrowserSelected;
-    this.firePropertyChange("showBrowserSelected", prevVal, showBrowserSelected);
+    this
+        .firePropertyChange("showBrowserSelected", prevVal, showBrowserSelected);
   }
 
-  public void setWorkspaceSelected(boolean workspaceSelected)
-  {
+  public void setWorkspaceSelected(boolean workspaceSelected) {
     boolean prevVal = this.workspaceSelected;
 
     this.workspaceSelected = workspaceSelected;
     this.firePropertyChange("workspaceSelected", prevVal, workspaceSelected);
   }
 
-  public void setContentEditEnabled(boolean enable)
-  {
+  public void setContentEditEnabled(boolean enable) {
     boolean prevVal = this.contentEditEnabled;
     contentEditEnabled = enable;
     this.firePropertyChange("contentEditEnabled", prevVal, contentEditEnabled);
   }
 
-  public void setContentEditSelected(boolean selected)
-  {
+  public void setContentEditSelected(boolean selected) {
     boolean prevVal = this.contentEditSelected;
     contentEditSelected = selected;
-    this.firePropertyChange("contentEditSelected", prevVal, contentEditSelected);
+    this
+        .firePropertyChange("contentEditSelected", prevVal, contentEditSelected);
   }
 
-  public boolean isContentEditSelected()
-  {
+  public boolean isContentEditSelected() {
     return this.contentEditSelected;
   }
 
-  public void setContentEditToggled()
-  {
+  public void setContentEditToggled() {
     setContentEditSelected(!this.contentEditSelected);
   }
 
-  public boolean isContentEditEnabled()
-  {
+  public boolean isContentEditEnabled() {
     return contentEditEnabled;
   }
 
