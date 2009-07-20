fc3135777c2ada14085c377073396fe3888910ba
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2009-07-20 16:17:35 +0000

    converted BaseMenuProviderTest to MicroPlatform
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@38526 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-plugin-services/plugin-mgr/test-src/org/pentaho/test/platform/plugin/pluginmgr/BaseMenuProviderTest.java
+++ bi-platform-plugin-services/plugin-mgr/test-src/org/pentaho/test/platform/plugin/pluginmgr/BaseMenuProviderTest.java
@@ -17,64 +17,69 @@
  */
 package org.pentaho.test.platform.plugin.pluginmgr;
 
-import java.io.File;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-import org.pentaho.platform.api.engine.IPentahoSession;
-import org.pentaho.platform.api.ui.IMenuProvider;
-import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.junit.Before;
+import org.junit.Test;
+import org.pentaho.platform.api.engine.IPluginManager;
+import org.pentaho.platform.api.engine.ISolutionEngine;
+import org.pentaho.platform.api.repository.ISolutionRepository;
 import org.pentaho.platform.engine.core.system.StandaloneSession;
-import org.pentaho.test.platform.engine.core.BaseTest;
+import org.pentaho.platform.engine.services.solution.SolutionEngine;
+import org.pentaho.platform.plugin.services.pluginmgr.DefaultPluginManager;
+import org.pentaho.platform.repository.solution.filebased.FileBasedSolutionRepository;
+import org.pentaho.test.platform.engine.core.MicroPlatform;
 
-public class BaseMenuProviderTest extends BaseTest {
-  private static final String SOLUTION_PATH = "plugin-mgr/test-res/solution3-menus"; //$NON-NLS-1$
-  private static final String ALT_SOLUTION_PATH = "test-res/solution3-menus"; //$NON-NLS-1$
-  private static final String PENTAHO_XML_PATH = "/system/pentahoObjects.spring.xml"; //$NON-NLS-1$
+@SuppressWarnings("nls")
+public class BaseMenuProviderTest {
+  
+  private MicroPlatform microPlatform;
+  private StandaloneSession session;
+  private MenuProvider menuProvider;
 
-  @Override 
-  public String getSolutionPath() {
-      File file = new File(SOLUTION_PATH + PENTAHO_XML_PATH);
-      if(file.exists()) {
-        return SOLUTION_PATH;  
-      } else {
-        return ALT_SOLUTION_PATH;
-      }
+  @Before
+  public void init0() {
+    microPlatform = new MicroPlatform("plugin-mgr/test-res/BaseMenuProviderTest/");
+    microPlatform.define(ISolutionEngine.class, SolutionEngine.class);
+    microPlatform.define(ISolutionRepository.class, FileBasedSolutionRepository.class);
+    microPlatform.define(IPluginManager.class, DefaultPluginManager.class);
+    microPlatform.init();
+    session = new StandaloneSession();
+    
+    menuProvider = new MenuProvider();
   }
 
+
+  @Test
   public void testMenu() throws Exception {
-	    startTest();
-	    
 	    String correctXulPath = "system/ui/menubar.xul"; //$NON-NLS-1$
 	    String badXulPath = "bogus"; //$NON-NLS-1$
 	    
-	    IPentahoSession session = new StandaloneSession( "test user" ); //$NON-NLS-1$
-	    IMenuProvider menu = PentahoSystem.get( IMenuProvider.class, session ); 
-	    assertNotNull( menu );
-	    
-	    Object obj = menu.getMenuBar("menu", correctXulPath, session); //$NON-NLS-1$
+	    Object obj = menuProvider.getMenuBar("menu", correctXulPath, session); //$NON-NLS-1$
 	    assertNotNull( obj );
-	    assertTrue( obj.toString().indexOf( "file-menu" ) > 0 ); //$NON-NLS-1$
+	    assertTrue("menu does not contain 'file-menu': ["+obj.toString()+"]", obj.toString().indexOf( "file-menu" ) > 0 ); //$NON-NLS-1$
 
-	    obj = menu.getMenuBar("bogus", correctXulPath, session); //$NON-NLS-1$
+	    obj = menuProvider.getMenuBar("bogus", correctXulPath, session); //$NON-NLS-1$
 	    assertNotNull( obj );
 	    assertEquals( "", obj.toString() ); //$NON-NLS-1$
 
-	    obj = menu.getMenuBar("menu", badXulPath, session); //$NON-NLS-1$
+	    obj = menuProvider.getMenuBar("menu", badXulPath, session); //$NON-NLS-1$
 	    assertNotNull( obj );
 	    assertEquals( "", obj.toString() ); //$NON-NLS-1$
 
-	    obj = menu.getPopupMenu( "testpopup" , correctXulPath, session); //$NON-NLS-1$
+	    obj = menuProvider.getPopupMenu( "testpopup" , correctXulPath, session); //$NON-NLS-1$
 	    assertNotNull( obj );
 	    assertTrue( obj.toString().indexOf( "test command" ) > 0 ); //$NON-NLS-1$
 
-	    obj = menu.getPopupMenu("bogus", correctXulPath, session); //$NON-NLS-1$
+	    obj = menuProvider.getPopupMenu("bogus", correctXulPath, session); //$NON-NLS-1$
 	    assertNotNull( obj );
 	    assertEquals( "", obj.toString() ); //$NON-NLS-1$
 
-	    obj = menu.getPopupMenu("testpopup", badXulPath, session); //$NON-NLS-1$
+	    obj = menuProvider.getPopupMenu("testpopup", badXulPath, session); //$NON-NLS-1$
 	    assertNotNull( obj );
 	    assertEquals( "", obj.toString() ); //$NON-NLS-1$
-
-	    finishTest();
 	  }
 
 }
