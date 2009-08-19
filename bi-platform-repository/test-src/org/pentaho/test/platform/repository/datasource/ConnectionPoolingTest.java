f53df279e9ac4cc5b9f2e3f89ad67af05370ef67
mlowery <mlowery@5d404267-db13-0410-8476-b1f6b398e63a>
2009-08-19 17:46:08 +0000

    Cleaned up imports.
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@39116 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-repository/test-src/org/pentaho/test/platform/repository/datasource/ConnectionPoolingTest.java
+++ bi-platform-repository/test-src/org/pentaho/test/platform/repository/datasource/ConnectionPoolingTest.java
@@ -19,13 +19,8 @@
 package org.pentaho.test.platform.repository.datasource;
 
 import java.io.File;
-import java.sql.Connection;
-import java.util.Timer;
-
-import javax.sql.DataSource;
 
 import org.pentaho.platform.engine.core.system.StandaloneSession;
-import org.pentaho.platform.engine.services.connection.datasource.dbcp.PooledDatasourceService;
 import org.pentaho.platform.engine.services.connection.datasource.dbcp.PooledDatasourceSystemListener;
 import org.pentaho.test.platform.engine.core.BaseTest;
 
