bf57cbd1a6827331d24381c34461cab3af5f19f3
mlowery <mlowery@5d404267-db13-0410-8476-b1f6b398e63a>
2009-09-03 16:27:07 +0000

    [PPP-2125] [PPP-2055]
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@39366 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-assembly/package-res-archive/scripts/tomcat/hsqldb/start-pentaho.bat
+++ bi-platform-assembly/package-res-archive/scripts/tomcat/hsqldb/start-pentaho.bat
@@ -1,18 +1,21 @@
 @echo off
 setlocal
-set JAVA_HOME=
 cscript promptuser.js //nologo //e:jscript
 rem errorlevel 0 means user chose "no"
 if %errorlevel%==0 goto quit
 echo WScript.Quit(1); > promptuser.js
 set PENTAHO_PATH=%~dp0
+if defined JAVA_HOME goto tomcat
+if defined JRE_HOME goto tomcat
+set JAVA_HOME=
 set JRE_HOME=%PENTAHO_PATH%jre
 set PATH=%JRE_HOME%\bin;%PATH%;
+:tomcat
 cd data
 start start_hypersonic.bat
 cd ..\tomcat\bin
 set CATALINA_HOME=%PENTAHO_PATH%tomcat
 set CATALINA_OPTS=-Xms256m -Xmx768m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000
 call startup
-endlocal
 :quit
+endlocal
--- bi-platform-assembly/package-res-archive/scripts/tomcat/mysql5/stop-pentaho.bat
+++ bi-platform-assembly/package-res-archive/scripts/tomcat/mysql5/stop-pentaho.bat
@@ -1,8 +1,12 @@
 @echo on
 setlocal
-set JAVA_HOME=
 set PENTAHO_PATH=%~dp0
+if defined JAVA_HOME goto tomcat
+if defined JRE_HOME goto tomcat
+set JAVA_HOME=
 set JRE_HOME=%PENTAHO_PATH%jre
+set PATH=%JRE_HOME%\bin;%PATH%;
+:tomcat
 cd tomcat\bin
 set CATALINA_HOME=%PENTAHO_PATH%tomcat
 shutdown.bat
