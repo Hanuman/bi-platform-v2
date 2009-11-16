3be45a44dc58033ea8f4a175ef7d0926411270bb
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2009-11-16 16:50:55 +0000

    added license headers
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@40168 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-test-foundation/src/org/pentaho/test/platform/engine/core/PluginManagerAdapter.java
+++ bi-platform-test-foundation/src/org/pentaho/test/platform/engine/core/PluginManagerAdapter.java
@@ -17,6 +17,24 @@ import org.pentaho.platform.api.engine.PluginBeanException;
 import org.pentaho.ui.xul.IMenuCustomization;
 import org.pentaho.ui.xul.XulOverlay;
 
+/*
+ * This program is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU General Public License, version 3 as published by the Free Software 
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this 
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl.html 
+ * or from the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2009 Pentaho Corporation.  All rights reserved. 
+ * 
+ */
 public class PluginManagerAdapter implements IPluginManager {
 
   public Object getBean(String beanId) throws PluginBeanException {
