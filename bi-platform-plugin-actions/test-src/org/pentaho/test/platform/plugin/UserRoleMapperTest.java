67bfff350831e605bbcc3f398543e6ce01c7e895
gmoran <gmoran@5d404267-db13-0410-8476-b1f6b398e63a>
2009-12-16 01:55:56 +0000

    BISERVER-3891 unit tests
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/branches/3.5@40418 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-plugin-actions/test-src/org/pentaho/test/platform/plugin/UserRoleMapperTest.java
+++ bi-platform-plugin-actions/test-src/org/pentaho/test/platform/plugin/UserRoleMapperTest.java
@@ -1,28 +1,36 @@
 package org.pentaho.test.platform.plugin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.pentaho.platform.api.data.IDatasourceService;
 import org.pentaho.platform.api.engine.IConnectionUserRoleMapper;
 import org.pentaho.platform.api.engine.ISolutionEngine;
+import org.pentaho.platform.api.engine.PentahoAccessControlException;
 import org.pentaho.platform.api.engine.IPentahoDefinableObjectFactory.Scope;
 import org.pentaho.platform.api.repository.ISolutionRepository;
+import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.core.system.StandaloneSession;
 import org.pentaho.platform.engine.core.system.boot.PlatformInitializationException;
 import org.pentaho.platform.engine.security.SecurityHelper;
 import org.pentaho.platform.engine.services.connection.datasource.dbcp.JndiDatasourceService;
 import org.pentaho.platform.engine.services.solution.SolutionEngine;
+import org.pentaho.platform.plugin.action.mondrian.catalog.IMondrianCatalogService;
 import org.pentaho.platform.plugin.action.mondrian.catalog.MondrianCatalog;
 import org.pentaho.platform.plugin.action.mondrian.catalog.MondrianCatalogHelper;
 import org.pentaho.platform.plugin.action.mondrian.catalog.MondrianSchema;
-import org.pentaho.platform.plugin.action.mondrian.mapper.MondrianAbstractPlatformUserRoleMapper;
 import org.pentaho.platform.plugin.action.mondrian.mapper.MondrianLookupMapUserRoleListMapper;
+import org.pentaho.platform.plugin.action.mondrian.mapper.MondrianOneToOneUserRoleListMapper;
+import org.pentaho.platform.plugin.action.mondrian.mapper.MondrianUserSessionUserRoleListMapper;
 import org.pentaho.platform.plugin.services.connections.mondrian.MDXConnection;
 import org.pentaho.platform.plugin.services.connections.sql.SQLConnection;
 import org.pentaho.platform.repository.solution.filebased.FileBasedSolutionRepository;
 import org.pentaho.test.platform.engine.core.MicroPlatform;
+import org.springframework.security.Authentication;
 import org.springframework.security.GrantedAuthority;
 import org.springframework.security.GrantedAuthorityImpl;
 import org.springframework.security.providers.UsernamePasswordAuthenticationToken;
@@ -36,6 +44,7 @@ public class UserRoleMapperTest {
     microPlatform = new MicroPlatform("test-src/solution");
     microPlatform.define(ISolutionEngine.class, SolutionEngine.class);
     microPlatform.define(ISolutionRepository.class, FileBasedSolutionRepository.class);
+    microPlatform.define(IMondrianCatalogService.class, MondrianCatalogHelper.class, Scope.GLOBAL);
     microPlatform.define("connection-SQL", SQLConnection.class);
     microPlatform.define("connection-MDX", MDXConnection.class);
     microPlatform.define(IDatasourceService.class, JndiDatasourceService.class, Scope.GLOBAL);
@@ -44,13 +53,11 @@ public class UserRoleMapperTest {
     } catch (PlatformInitializationException ex) {
       Assert.fail();
     }
-    
-    // Datasources
-    MondrianCatalogHelper helper = MondrianCatalogHelper.getInstance();
-//    helper.setDataSourcesConfig("file:test/analysis/test-datasources.xml"); //$NON-NLS-1$
-    helper.setDataSourcesConfig("file:" + //$NON-NLS-1$
-        PentahoSystem.getApplicationContext().getSolutionPath("test/analysis/test-datasources.xml")); //$NON-NLS-1$
 
+    MondrianCatalogHelper catalogService = (MondrianCatalogHelper)PentahoSystem.get(IMondrianCatalogService.class);
+    catalogService.setDataSourcesConfig("file:" + //$NON-NLS-1$
+        PentahoSystem.getApplicationContext().getSolutionPath("test/analysis/test-datasources.xml")); //$NON-NLS-1$
+    
     // JNDI
     System.setProperty("java.naming.factory.initial", "org.osjava.sj.SimpleContextFactory"); //$NON-NLS-1$ //$NON-NLS-2$
     System.setProperty("org.osjava.sj.root", "test-src/solution/system/simple-jndi"); //$NON-NLS-1$ //$NON-NLS-2$
@@ -63,7 +70,7 @@ public class UserRoleMapperTest {
     StandaloneSession session;
     session = new StandaloneSession();
     UserRoleMapperTest.createJoe(session);
-    MondrianCatalogHelper helper = MondrianCatalogHelper.getInstance();
+    MondrianCatalogHelper helper = (MondrianCatalogHelper)PentahoSystem.get(IMondrianCatalogService.class);;
     Assert.assertNotNull(helper);
     MondrianCatalog mc = helper.getCatalog("SteelWheelsRoles", session);
     Assert.assertNotNull(mc);
@@ -77,22 +84,145 @@ public class UserRoleMapperTest {
   }
   
   @Test
-  public void testLookupMapUserRoleListMapper() {
-    IConnectionUserRoleMapper mapper = new MondrianLookupMapUserRoleListMapper();
+  public void testReadRolesInPlatform() {
+    StandaloneSession session;
+    session = new StandaloneSession();
+    UserRoleMapperTest.createJoe(session);
     
+    Authentication auth = SecurityHelper.getAuthentication(session, false);
+    Assert.assertNotNull(auth);
+    GrantedAuthority[] gAuths = auth.getAuthorities();
+    Assert.assertNotNull(gAuths);
+    Assert.assertEquals(3, gAuths.length);
+    Assert.assertEquals("ceo", gAuths[0].getAuthority());
+    Assert.assertEquals("Admin", gAuths[1].getAuthority());
+    Assert.assertEquals("Authenticated", gAuths[2].getAuthority());
+
+  }
+
+  @Test
+  public void testMondrianUserSessionUserRoleListMapper() {
+    StandaloneSession session;
+    session = new StandaloneSession();
+    UserRoleMapperTest.createJoe(session);
+    
+    session.setAttribute("rolesAttribute", new Object[]{"mondrianRole1", "mondrianRole2", "mondrianRole3"});
+    PentahoSessionHolder.setSession(session);
+    
+    MondrianUserSessionUserRoleListMapper mapper = new MondrianUserSessionUserRoleListMapper();
+    mapper.setSessionProperty("rolesAttribute");
+
+    try {
+      String[] roles = mapper.mapConnectionRoles(session, "SteelWheelsRoles");
+      Assert.assertNotNull(roles);
+      Assert.assertEquals(3, roles.length);
+      Assert.assertEquals("mondrianRole1", roles[0]);
+      Assert.assertEquals("mondrianRole2", roles[1]);
+      Assert.assertEquals("mondrianRole3", roles[2]);
+    } catch (PentahoAccessControlException e) {
+      Assert.fail(e.getMessage());
+    }
+  }
+
+  @Test
+  public void testNoMatchMondrianUserSessionUserRoleListMapper() {
+    StandaloneSession session;
+    session = new StandaloneSession();
+    UserRoleMapperTest.createJoe(session);
+    
+    PentahoSessionHolder.setSession(session);
+    
+    MondrianUserSessionUserRoleListMapper mapper = new MondrianUserSessionUserRoleListMapper();
+    mapper.setSessionProperty("rolesAttribute");
+
+    try {
+      String[] roles = mapper.mapConnectionRoles(session, "SteelWheelsRoles");
+      Assert.assertNull(roles);
+    } catch (PentahoAccessControlException e) {
+      Assert.fail(e.getMessage());
+    }
+  }
+
+  @Test
+  public void testLookupMapUserRoleListMapper() {
+    StandaloneSession session;
+    session = new StandaloneSession();
+    UserRoleMapperTest.createJoe(session);
+    
+    Map<String, String> lookup = new HashMap<String, String>();
+    lookup.put("ceo", "Role1");
+    lookup.put("Not Pentaho","Role2");
+    lookup.put("Not Mondrian or Pentaho","Role3");
+    
+    MondrianLookupMapUserRoleListMapper mapper = new MondrianLookupMapUserRoleListMapper();
+    mapper.setLookupMap(lookup);
+
+    try {
+      String[] roles = mapper.mapConnectionRoles(session, "SteelWheelsRoles");
+      Assert.assertNotNull(roles);
+      Assert.assertEquals(1, roles.length);
+      Assert.assertEquals("Role1", roles[0]);
+    } catch (PentahoAccessControlException e) {
+      Assert.fail(e.getMessage());
+    }
   }
   
-//  public String[] resolveRoles(MondrianAbstractPlatformUserRoleMapper mapper, int flag) {
-//    mapper.
-//  }
-
-  public class TestPlatformLookupMapUserRoleMapper extends MondrianLookupMapUserRoleListMapper {
-
-    protected String[] mapRoles(String[] mondrianRoles, String[] platformRoles, String mondrianCatalog) {
-      return super.mapRoles(mondrianRoles, platformRoles, mondrianCatalog);
-    }
+  @Test
+  public void testNoMatchLookupMapUserRoleListMapper() {
+    StandaloneSession session;
+    session = new StandaloneSession();
+    UserRoleMapperTest.createJoe(session);
     
+    Map<String, String> lookup = new HashMap<String, String>();
+    lookup.put("No Match", "Role1");
+    lookup.put("No Match Here Either","Role2");
+    
+    MondrianLookupMapUserRoleListMapper mapper = new MondrianLookupMapUserRoleListMapper();
+    mapper.setLookupMap(lookup);
+
+    try {
+      String[] roles = mapper.mapConnectionRoles(session, "SteelWheelsRoles");
+      Assert.assertNull(roles);
+    } catch (PentahoAccessControlException e) {
+      Assert.fail(e.getMessage());
+    }
   }
+
+  @Test
+  public void testMondrianOneToOneUserRoleListMapper() {
+    StandaloneSession session;
+    session = new StandaloneSession();
+    UserRoleMapperTest.createSimpleBob(session);
+
+    IConnectionUserRoleMapper mapper = new MondrianOneToOneUserRoleListMapper();
+    try {
+      String[] roles = mapper.mapConnectionRoles(session, "SteelWheelsRoles");
+      Assert.assertNotNull(roles);
+      Assert.assertEquals(2, roles.length);
+      Assert.assertEquals("Role1", roles[0]);
+      Assert.assertEquals("Role2", roles[1]);
+      
+    } catch (PentahoAccessControlException e) {
+      Assert.fail(e.getMessage());
+    }
+  }
+  
+  @Test
+  public void testNoMatchMondrianOneToOneUserRoleListMapper() {
+    StandaloneSession session;
+    session = new StandaloneSession();
+    UserRoleMapperTest.createJoe(session);
+
+    IConnectionUserRoleMapper mapper = new MondrianOneToOneUserRoleListMapper();
+    try {
+      String[] roles = mapper.mapConnectionRoles(session, "SteelWheelsRoles");
+      Assert.assertNull(roles);
+      
+    } catch (PentahoAccessControlException e) {
+      Assert.fail(e.getMessage());
+    }
+  }
+
   
   public static void createSuzy(StandaloneSession session) {
     session.setAuthenticated("suzy"); //$NON-NLS-1$
@@ -102,7 +232,6 @@ public class UserRoleMapperTest {
     auths[2] = new GrantedAuthorityImpl("ROLE_AUTHENTICATED"); //$NON-NLS-1$
     UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken("suzy", "none", auths //$NON-NLS-1$ //$NON-NLS-2$
     );
-    auth.setAuthenticated(true);
     // We now have a credential. We need to bind it into the IPentahoSession
     SecurityHelper.setPrincipal(auth, session);
     // We should be good to go now...
@@ -116,10 +245,21 @@ public class UserRoleMapperTest {
     auths[2] = new GrantedAuthorityImpl("Authenticated"); //$NON-NLS-1$
     UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken("joe", "none", auths //$NON-NLS-1$ //$NON-NLS-2$
     );
-    auth.setAuthenticated(true);
     // We now have a credential. We need to bind it into the IPentahoSession
     SecurityHelper.setPrincipal(auth, session);
     // We should be good to go now...
   }
  
+  public static void createSimpleBob(StandaloneSession session) {
+    session.setAuthenticated("simplebob"); //$NON-NLS-1$
+    GrantedAuthority[] auths = new GrantedAuthority[2];
+    auths[0] = new GrantedAuthorityImpl("Role1"); //$NON-NLS-1$
+    auths[1] = new GrantedAuthorityImpl("Role2"); //$NON-NLS-1$
+    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken("simplebob", "none", auths //$NON-NLS-1$ //$NON-NLS-2$
+    );
+    // We now have a credential. We need to bind it into the IPentahoSession
+    SecurityHelper.setPrincipal(auth, session);
+    // We should be good to go now...
+  }
+
 }
