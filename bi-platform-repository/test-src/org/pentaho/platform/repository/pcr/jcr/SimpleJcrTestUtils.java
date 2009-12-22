3a0d294ce07aa82e47ee50ff6ec4eb61d1390d85
mlowery <mlowery@5d404267-db13-0410-8476-b1f6b398e63a>
2009-12-22 20:24:05 +0000

    [BISERVER-3879] refactoring
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/trunk@40462 5d404267-db13-0410-8476-b1f6b398e63a

---
+++ bi-platform-repository/test-src/org/pentaho/platform/repository/pcr/jcr/SimpleJcrTestUtils.java
@@ -0,0 +1,164 @@
+package org.pentaho.platform.repository.pcr.jcr;
+
+import java.util.Date;
+
+import javax.jcr.Item;
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.version.VersionHistory;
+import javax.jcr.version.VersionIterator;
+
+import org.apache.jackrabbit.api.jsr283.security.AccessControlPolicy;
+import org.apache.jackrabbit.api.jsr283.security.AccessControlPolicyIterator;
+import org.apache.jackrabbit.api.jsr283.security.Privilege;
+import org.apache.jackrabbit.core.SessionImpl;
+import org.pentaho.platform.repository.pcr.jcr.PentahoJcrConstants;
+import org.springframework.extensions.jcr.JcrCallback;
+import org.springframework.extensions.jcr.JcrTemplate;
+import org.springframework.util.Assert;
+
+public class SimpleJcrTestUtils {
+
+  public static void deleteItem(final JcrTemplate jcrTemplate, final String absPath) {
+    jcrTemplate.execute(new JcrCallback() {
+      public Object doInJcr(final Session session) throws RepositoryException {
+        Item item;
+        try {
+          item = session.getItem(absPath);
+        } catch (PathNotFoundException e) {
+          return null;
+        }
+        item.remove();
+        session.save();
+        return null;
+      }
+    });
+  }
+
+  public static String addNode(final JcrTemplate jcrTemplate, final String parentAbsPath, final String name,
+      final String primaryNodeTypeName) {
+    return (String) jcrTemplate.execute(new JcrCallback() {
+      public String doInJcr(final Session session) throws RepositoryException {
+        Node newNode;
+        try {
+          Item item = session.getItem(parentAbsPath);
+          Assert.isTrue(item.isNode());
+          Node parentNode = (Node) item;
+          newNode = parentNode.addNode(name, primaryNodeTypeName);
+          newNode.addMixin(PentahoJcrConstants.MIX_REFERENCEABLE);
+        } catch (PathNotFoundException e) {
+          return null;
+        }
+        session.save();
+        return newNode.getUUID();
+      }
+    });
+  }
+
+  public static Item getItem(final JcrTemplate jcrTemplate, final String absPath) {
+    return (Item) jcrTemplate.execute(new JcrCallback() {
+      public Object doInJcr(final Session session) throws RepositoryException {
+        Item item;
+        try {
+          item = session.getItem(absPath);
+        } catch (PathNotFoundException e) {
+          return null;
+        }
+        return item;
+      }
+    });
+  }
+
+  public static String getNodeId(final JcrTemplate jcrTemplate, final String absPath) {
+    return (String) jcrTemplate.execute(new JcrCallback() {
+      public Object doInJcr(final Session session) throws RepositoryException {
+        Item item;
+        try {
+          item = session.getItem(absPath);
+        } catch (PathNotFoundException e) {
+          return null;
+        }
+        Assert.isTrue(item.isNode());
+        return ((Node) item).getUUID();
+      }
+    });
+  }
+
+  public static int getVersionCount(final JcrTemplate jcrTemplate, final String absPath) {
+    return (Integer) jcrTemplate.execute(new JcrCallback() {
+      public Object doInJcr(final Session session) throws RepositoryException {
+        Node fileNode = (Node) session.getItem(absPath);
+        VersionHistory versionHistory = fileNode.getVersionHistory();
+        VersionIterator versionIterator = versionHistory.getAllVersions();
+        int versionCount = 0;
+        while (versionIterator.hasNext()) {
+          versionIterator.nextVersion();
+          versionCount++;
+        }
+        return versionCount;
+      }
+    });
+  }
+
+  public static void printAccess(final JcrTemplate jcrTemplate, final String absPath) {
+    jcrTemplate.execute(new JcrCallback() {
+      public Object doInJcr(final Session session) throws RepositoryException {
+
+        SessionImpl jrSession = (SessionImpl) session;
+        AccessControlPolicy[] epols = jrSession.getAccessControlManager().getEffectivePolicies(absPath);
+        AccessControlPolicy[] pols = jrSession.getAccessControlManager().getPolicies(absPath);
+        AccessControlPolicyIterator apols = jrSession.getAccessControlManager().getApplicablePolicies(absPath);
+        return null;
+      }
+    });
+  }
+
+  public static boolean hasPrivileges(final JcrTemplate jcrTemplate, final String absPath, final String... privNames) {
+    return (Boolean) jcrTemplate.execute(new JcrCallback() {
+      public Object doInJcr(final Session session) throws RepositoryException {
+        Assert.notEmpty(privNames);
+        SessionImpl jrSession = (SessionImpl) session;
+        Privilege[] privs = new Privilege[privNames.length];
+        for (int i = 0; i < privs.length; i++) {
+          privs[i] = jrSession.getAccessControlManager().privilegeFromName(privNames[i]);
+        }
+        return jrSession.getAccessControlManager().hasPrivileges(absPath, privs);
+      }
+    });
+
+  }
+  
+  public static boolean isLocked(final JcrTemplate jcrTemplate, final String absPath) {
+    return (Boolean) jcrTemplate.execute(new JcrCallback() {
+      public Object doInJcr(final Session session) throws RepositoryException {
+        Item item = session.getItem(absPath);
+        Assert.isTrue(item.isNode());
+        return ((Node) item).isLocked();
+      }
+    });
+  }
+  
+  public static String getString(final JcrTemplate jcrTemplate, final String absPath) {
+    return (String) jcrTemplate.execute(new JcrCallback() {
+      public Object doInJcr(final Session session) throws RepositoryException {
+        Item item = session.getItem(absPath);
+        Assert.isTrue(!item.isNode());
+        return ((Property) item).getString();
+      }
+    });
+  }
+  
+  public static Date getDate(final JcrTemplate jcrTemplate, final String absPath) {
+    return (Date) jcrTemplate.execute(new JcrCallback() {
+      public Object doInJcr(final Session session) throws RepositoryException {
+        Item item = session.getItem(absPath);
+        Assert.isTrue(!item.isNode());
+        return ((Property) item).getDate().getTime();
+      }
+    });
+  }
+
+}
---
+++ bi-platform-repository/test-src/org/pentaho/platform/repository/pcr/jcr/jackrabbit/TestPrincipalProvider.java
@@ -0,0 +1,129 @@
+package org.pentaho.platform.repository.pcr.jcr.jackrabbit;
+
+import java.security.Principal;
+import java.security.acl.Group;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.jcr.Session;
+
+import org.apache.jackrabbit.api.security.principal.PrincipalIterator;
+import org.apache.jackrabbit.api.security.principal.PrincipalManager;
+import org.apache.jackrabbit.core.security.AnonymousPrincipal;
+import org.apache.jackrabbit.core.security.UserPrincipal;
+import org.apache.jackrabbit.core.security.principal.AdminPrincipal;
+import org.apache.jackrabbit.core.security.principal.EveryonePrincipal;
+import org.apache.jackrabbit.core.security.principal.PrincipalIteratorAdapter;
+import org.apache.jackrabbit.core.security.principal.PrincipalProvider;
+import org.pentaho.platform.repository.pcr.jcr.jackrabbit.SpringSecurityGrantedAuthorityPrincipal;
+
+/**
+ * PrincipalProvider for unit test purposes. Has joe and the other Pentaho users. In addition, it has "everyone", 
+ * "admin", and "anonymous".
+ * 
+ * <p>
+ * Some parts copied from SimplePrincipalProvider.
+ * </p>
+ * 
+ * @author mlowery
+ */
+public class TestPrincipalProvider implements PrincipalProvider {
+
+  private final Map<String, Principal> principals = new HashMap<String, Principal>();
+
+  public TestPrincipalProvider() {
+    principals.put("admin", new AdminPrincipal("admin"));
+    principals.put("anonymous", new AnonymousPrincipal());
+
+    EveryonePrincipal everyone = EveryonePrincipal.getInstance();
+    principals.put(everyone.getName(), everyone);
+
+    principals.put("joe", new UserPrincipal("joe"));
+    principals.put("suzy", new UserPrincipal("suzy"));
+    principals.put("tiffany", new UserPrincipal("tiffany"));
+    principals.put("pat", new UserPrincipal("pat"));
+    principals.put("Authenticated", new SpringSecurityGrantedAuthorityPrincipal("Authenticated"));
+    principals.put("acme_Authenticated", new SpringSecurityGrantedAuthorityPrincipal("acme_Authenticated"));
+    principals.put("acme_Admin", new SpringSecurityGrantedAuthorityPrincipal("acme_Admin"));
+    principals.put("duff_Authenticated", new SpringSecurityGrantedAuthorityPrincipal("duff_Authenticated"));
+  }
+
+  public Principal getPrincipal(String principalName) {
+    if (principals.containsKey(principalName)) {
+      return (Principal) principals.get(principalName);
+    } else {
+      return null;
+    }
+  }
+
+  public PrincipalIterator findPrincipals(String simpleFilter) {
+    return findPrincipals(simpleFilter, PrincipalManager.SEARCH_TYPE_ALL);
+  }
+
+  public PrincipalIterator findPrincipals(String simpleFilter, int searchType) {
+    Principal p = getPrincipal(simpleFilter);
+    if (p == null) {
+      return PrincipalIteratorAdapter.EMPTY;
+    } else if (p instanceof Group && searchType == PrincipalManager.SEARCH_TYPE_NOT_GROUP || !(p instanceof Group)
+        && searchType == PrincipalManager.SEARCH_TYPE_GROUP) {
+      return PrincipalIteratorAdapter.EMPTY;
+    } else {
+      return new PrincipalIteratorAdapter(Collections.singletonList(p));
+    }
+  }
+
+  public PrincipalIterator getPrincipals(int searchType) {
+    PrincipalIterator it;
+    switch (searchType) {
+      case PrincipalManager.SEARCH_TYPE_GROUP:
+        it = new PrincipalIteratorAdapter(Collections.singletonList(EveryonePrincipal.getInstance()));
+        break;
+      case PrincipalManager.SEARCH_TYPE_NOT_GROUP:
+        Set<Principal> set = new HashSet<Principal>(principals.values());
+        set.remove(EveryonePrincipal.getInstance());
+        it = new PrincipalIteratorAdapter(set);
+        break;
+      case PrincipalManager.SEARCH_TYPE_ALL:
+        it = new PrincipalIteratorAdapter(principals.values());
+        break;
+      // no default
+      default:
+        throw new IllegalArgumentException("Unknown search type " + searchType);
+    }
+    return it;
+  }
+
+  public PrincipalIterator getGroupMembership(Principal principal) {
+    if (principal instanceof EveryonePrincipal) {
+      return PrincipalIteratorAdapter.EMPTY;
+    }
+
+    Set<Principal> principals = new HashSet<Principal>();
+    if (principal.getName().equals("joe") || principal.getName().equals("suzy")
+        || principal.getName().equals("tiffany")) {
+      principals.add(new SpringSecurityGrantedAuthorityPrincipal("Authenticated"));
+      principals.add(new SpringSecurityGrantedAuthorityPrincipal("acme_Authenticated"));
+    } else if (principal.getName().equals("pat")) {
+      principals.add(new SpringSecurityGrantedAuthorityPrincipal("Authenticated"));
+      principals.add(new SpringSecurityGrantedAuthorityPrincipal("duff_Authenticated"));
+    }
+    return new PrincipalIteratorAdapter(principals);
+  }
+
+  public void init(Properties options) {
+    // nothing to do
+  }
+
+  public void close() {
+    // nothing to do
+  }
+
+  public boolean canReadPrincipal(Session session, Principal principal) {
+    return true;
+  }
+
+}
