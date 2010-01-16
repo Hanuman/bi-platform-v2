b489dc07773f948c82282e86a25c33f268a80499
aphillips <aphillips@5d404267-db13-0410-8476-b1f6b398e63a>
2010-01-16 01:10:13 +0000

    [BUILD] - merged in Xul fix
    git-svn-id: http://source.pentaho.org/svnroot/bi-platform-v2/branches/3.5.2-GA@40677 5d404267-db13-0410-8476-b1f6b398e63a

--- bi-platform-data-access/src/org/pentaho/platform/dataaccess/datasource/wizard/controllers/CustomAggregateCellEditor.java
+++ bi-platform-data-access/src/org/pentaho/platform/dataaccess/datasource/wizard/controllers/CustomAggregateCellEditor.java
@@ -90,7 +90,7 @@ public class CustomAggregateCellEditor extends XulEventSourceAdapter implements 
     List<AggregationType> aggTypeList = new ArrayList<AggregationType>();
     for (XulCheckbox checkbox : aggregationCheckboxList) {
       if (checkbox.isChecked()) {
-        aggTypeList.add(AggregationType.valueOf(checkbox.getID()));
+        aggTypeList.add(AggregationType.valueOf(checkbox.getId()));
       }
     }
     aggregation.setAggregationList(aggTypeList);
@@ -119,7 +119,7 @@ public class CustomAggregateCellEditor extends XulEventSourceAdapter implements 
         XulCheckbox aggregationCheckBox;
         aggregationCheckBox = (XulCheckbox) document.createElement("checkbox");//$NON-NLS-1$
         aggregationCheckBox.setLabel(datasourceMessages.getString(aggregationTypeArray[i].getDescription()));
-        aggregationCheckBox.setID(aggregationTypeArray[i].name());
+        aggregationCheckBox.setId(aggregationTypeArray[i].name());
         aggregationCheckBox.setCommand(null);
         if (aggregationList.contains(aggregationTypeArray[i])) {
           aggregationCheckBox.setChecked(true);
@@ -134,7 +134,7 @@ public class CustomAggregateCellEditor extends XulEventSourceAdapter implements 
         XulCheckbox aggregationCheckBox;
         aggregationCheckBox = (XulCheckbox) document.createElement("checkbox"); //$NON-NLS-1$
         aggregationCheckBox.setLabel(datasourceMessages.getString(aggregationTypeArray[j].getDescription()));
-        aggregationCheckBox.setID(aggregationTypeArray[j].name());
+        aggregationCheckBox.setId(aggregationTypeArray[j].name());
         aggregationCheckBox.setCommand(null);
         if (aggregationList.contains(aggregationTypeArray[j])) {
           aggregationCheckBox.setChecked(true);
@@ -158,7 +158,7 @@ public class CustomAggregateCellEditor extends XulEventSourceAdapter implements 
       listbox = (XulMenuList) document.createElement("menulist");//$NON-NLS-1$
       XulMenupopup menuPopup = (XulMenupopup) document.createElement("menupopup");//$NON-NLS-1$
       listbox.addChild(menuPopup);
-      listbox.setID("DefaultAggregationType");//$NON-NLS-1$
+      listbox.setId("DefaultAggregationType");//$NON-NLS-1$
       listbox.setBinding("name");//$NON-NLS-1$
       dialog.addChild(label);
       dialog.addChild(listbox);
@@ -246,7 +246,7 @@ public class CustomAggregateCellEditor extends XulEventSourceAdapter implements 
       if (component instanceof XulCheckbox) {
         XulCheckbox checkbox = (XulCheckbox) component;
         if (checkbox.isChecked()) {
-          aggregationTypeList.add(AggregationType.valueOf(checkbox.getID()));
+          aggregationTypeList.add(AggregationType.valueOf(checkbox.getId()));
         }
       }
     }
@@ -254,7 +254,7 @@ public class CustomAggregateCellEditor extends XulEventSourceAdapter implements 
       if (component instanceof XulCheckbox) {
         XulCheckbox checkbox = (XulCheckbox) component;
         if (checkbox.isChecked()) {
-          aggregationTypeList.add(AggregationType.valueOf(checkbox.getID()));
+          aggregationTypeList.add(AggregationType.valueOf(checkbox.getId()));
         }
       }
     }    
